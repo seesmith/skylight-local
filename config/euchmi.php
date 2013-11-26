383788d41ccd482850891daa43a3136b549c7d41
Scott Renton <scott.renton@ed.ac.uk>
2013-11-26 11:12:32 +0000

    Including most recent pull request

--- config/euchmi.php
+++ config/euchmi.php
@@ -15,38 +15,26 @@ $config['skylight_oaipmhcollection'] = 'hdl_123456789_4';
 $config['skylight_container_id'] = '11';
 $config['skylight_container_field'] = 'location.coll';
 
-<<<<<<< HEAD
+
 $config['skylight_fields'] = array('Title' => 'dc.title.en_US',
     'Author' => 'dc.contributor.author.en_US',
     'Country' => 'lido.country.en_US',
     'Subject' => 'dc.subject.en_US',
     'Instrument' => 'dc.type.en_US',
     'Abstract' => 'dc.description.abstract.en_US',
-=======
-$config['skylight_fields'] = array('Title' => 'dc.title.en',
-    'Author' => 'dc.contributor.author',
-    'Country' => 'lido.country.en',
-    'Subject' => 'dc.subject.en',
-    'Instrument' => 'dc.type.en',
-    'Abstract' => 'dc.description.abstract.en',
->>>>>>> upstream/master
     'Date' => 'dc.date.issued',
     'Bitstream'=> 'dc.format.original',
     'Thumbnail'=> 'dc.format.thumbnail',
-    'Place Made' => 'dc.coverage.spatial.en',
+    'Place Made' => 'dc.coverage.spatial.en_US',
     'Date Made' => 'dc.date.created',
     'Accession Number' => 'dc.identifier.other',
-    'Description' => 'dc.description.en',
-    'Collection' => 'dc.relation.ispartof.en'
+    'Description' => 'dc.description.en_US',
+    'Collection' => 'dc.relation.ispartof.en_US'
 );
 
 
 $config['skylight_date_filters'] = array('Date' => 'dateIssued.year_sort');
 $config['skylight_filters'] = array('Collection'=> 'collection_filter', 'Maker' => 'author_filter', 'Place Made' => 'place_filter', 'Instrument Type' => 'type_filter');
-<<<<<<< HEAD
-
-=======
->>>>>>> upstream/master
 $config['skylight_filter_delimiter'] = ':';
 
 $config['skylight_meta_fields'] = array('Title' => 'dc.title',
