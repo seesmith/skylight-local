dd79a2cf4af91942eced181117e9e157e0c673b3
Ianthe Sutherland <ianthe88@gmail.com>
2014-03-14 16:49:38 +0000

    LIB001-562: Header, footer and artwork and CSS for Geology

--- theme/rocks/views/header.php
+++ theme/rocks/views/header.php
@@ -1,9 +1,9 @@
 <!doctype html>
-<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
-<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+    <!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+    <!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
+    <!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
+    <!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
+    <!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
@@ -18,7 +18,6 @@
         Remove this if you use the .htaccess -->
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-
         <title>Cockburn Geological Collection</title>
 
         <meta name="description" content="">
@@ -28,9 +27,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <!-- Place favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
-        <link rel="shortcut icon" href="./favicon.ico">
-        <link rel="apple-touch-icon" href="./apple-touch-icon.png">
-
+        <link rel="shortcut icon" href="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/favicon.ico">
+        <link rel="apple-touch-icon" href="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/apple-touch-icon.png">
 
         <!-- CSS: implied media="all" -->
         <link rel="stylesheet" href="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/css/style.css?v=2">
@@ -42,9 +40,9 @@
         <script src="<?php echo base_url(); ?>assets/modernizr/modernizr-1.7.min.js"></script>
 
         <?php if (isset($solr)) { ?><link rel="schema.DC" href="http://purl.org/dc/elements/1.1/" />
-        <link rel="schema.DCTERMS" href="http://purl.org/dc/terms/" />
+            <link rel="schema.DCTERMS" href="http://purl.org/dc/terms/" />
 
-        <?php
+            <?php
 
             foreach($metafields as $label => $element) {
                 $field = "";
@@ -65,21 +63,22 @@
 
     <body>
 
-        <div id="container">
-            <header>
-                <p class="collection-title"><img src = "<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/cockburn-header.jpg" width="940px" ></p>
-                 <form action="./redirect/" method="post">
+    <div id="container">
+        <header>
+            <div id="collection-title">
+                <a href="http://www.ed.ac.uk" class="uoelogo" title="The University of Edinburgh" target="_blank"></a>
+                <a href="<?php echo base_url(); ?>" class="geologylogo" title="Cockburn Geological Collection"></a>
+                <a href="<?php echo base_url(); ?>" class="menulogo" title="Items from the Cockburn Geological Collection"></a>
+            </div>
+            <div id="collection-search">
+                <form action="./redirect/" method="post">
                     <fieldset class="search">
                         <input type="text" name="q" value="<?php if (isset($searchbox_query)) echo urldecode($searchbox_query); ?>" id="q" />
                         <input type="submit" name="submit_search" class="btn" value="Search" id="submit_search" />
                         <a href="./advanced" class="advanced">Advanced search</a>
                     </fieldset>
                 </form>
-                <nav class="header-links">
-                    <a href="../">Home</a>
-                    <a href="./about/">About this site</a>
-                    <a href="./feedback/" class="last">Feedback</a>
-                </nav>
-            </header>
+            </div>
+        </header>
 
-            <div id="main" role="main" class="clearfix">
+        <div id="main" role="main" class="clearfix">
