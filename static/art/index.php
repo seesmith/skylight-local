678ef8e26b1ca114fc5813a75528a66258c4c6f8
Claire Knowles <cgknowles@gmail.com>
2014-03-20 14:15:49 +0000

    LIB001-578 Art Index page text

--- static/art/index.php
+++ static/art/index.php
@@ -1,3 +1,35 @@
 <div class="record">
-    <p>The University holds around 2500 works of art. The Art Collection is now an amalgamation of the University of Edinburgh’s original art collection, which spans some 400 years of collecting, and the Edinburgh College of Art collection of prints, drawings, paintings and sculpture. The ECA collection tells a unique story of the artist output of the College, particularly in the early to mid-20th Century. Some of the most respected names is Scottish art appear, such as Samuel Peploe, John Bellany, Anne Redpath and Elizabeth Blackadder.</p>
+    <p>The University holds around 2500 works of art. The Art Collection is now an amalgamation of the University of
+        Edinburgh’s original art collection, which spans some 400 years of collecting, and the Edinburgh College of Art
+        collection of prints, drawings, paintings and sculpture. The ECA collection tells a unique story of the artist
+        output of the College, particularly in the early to mid-20th Century. Some of the most respected names is Scottish
+        art appear, such as <a href="./search/Peploe">Samuel Peploe</a>, <a href="./search/Bellany">John Bellany</a>,
+        <a href="./search/Redpath">Anne Redpath</a> and <a href="./search/Blackadder">Elizabeth Blackadder</a>.</p>
+    <p>
+        The Art Collection has as its centrepiece the Torrie Collection of seventeenth-century Dutch and Italian Masters
+        with works by <a href="./search/Ruisdael">Ruisdael</a>, <a href="./search/ten%20Oever">ten Oever</a>,
+        <a href="./search/Van%20der%20Meulen">Van der Meulen</a>, <a href="./search/Pynacker">Pynacker</a>,
+        <a href="./search/Rosa">Rosa</a> and <a href="./search/Rvan%20de%20Velde">Rvan de Velde</a>. The University also holds
+        the second largest collection of portraits in Scotland ranging from seventeenth-century portraits of
+        <a href="./search/Napier">John Napier</a> and <a href="./search/John%20Knox">John Knox</a>
+        to the recent dynamic painting of <a href="./search/Peter%20Higgs">Peter Higgs</a>
+        by Ken Currie. Other portrait artists represented are
+        <a href="./search/Henry%20Raeburn">Sir Henry Raeburn</a>, <a href="./search/Stanley%20Cursiter">Stanley Cursiter</a>,
+        <a href="./search/George%20Reid">Sir George Reid</a>, <a href="./search/James%20Cowie">James Cowie</a> and
+        <a href="./search/Victoria%20Crowe">Victoria Crowe</a>.
+    </p>
+    <p>
+        A large percentage of the Art Collection is on display enhancing the public, staff and student spaces of the
+        University. A number of works from the Torrie Collection are on long-term loan to the National Galleries of Scotland.
+        The principal areas of display include Edinburgh College of Art, Old College, Playfair Library, Raeburn Room,
+        the University of Edinburgh Library, New College, McEwan Hall and the Royal (Dick) School of Veterinary Studies.
+    </p>
+    <p>
+        As well as supporting research at all levels of the University, we believe that the uniqueness of our art collections
+        lies in their innovative use. We seek to interpret and display collections in new and exciting ways, introducing
+        contemporary insights and conversations to constantly reinvigorate debate. This involves working closely with
+        Edinburgh College of Art and The Talbot Rice Gallery to ensure that collections are developed through contemporary
+        collecting and also utilised by some of the world’s leading contemporary artists, academics and curators and
+        presented to as wide an audience as possible.
+    </p>
 </div>
