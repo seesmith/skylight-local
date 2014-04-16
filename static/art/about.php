9932adff020d8de29629b2d69816a0ac76ab7d4e
Claire Knowles <claire.knowles@ed.ac.uk>
2014-04-16 15:06:23 +0100

    LIB001-645 Change Contact link to Feedback and add contact details to about pages

---
+++ static/art/about.php
@@ -0,0 +1,39 @@
+<div class="content">
+    <div class="content byEditor">
+        <h1>Holding text to be revisited for launch</h1>
+        <h2>About the University of Edinburgh Art Collection</h2>
+        <p>
+            Info about the collection
+        </p>
+
+        <h2>Art Collections Online</h2>
+        <p>
+            Info about this site
+        </p>
+        <h2>University Collections</h2>
+        <p>
+            Info about the University Collections link to collections.ed.ac.uk
+        </p>
+        <h2>BBC Your Paintings</h2>
+        <p>
+        <a href="http://www.bbc.co.uk/arts/yourpaintings/galleries/locations/university-of-edinburgh-5095" title="" >University Collections on BBC Your Paintings</a><br/>
+        <a href="http://www.bbc.co.uk/arts/yourpaintings/galleries/locations/eca-part-of-university-of-edinburgh-fine-art-collection-7553" title="" >Edinburgh College of Art Collections on BBC Your Paintings</a>
+        <p/>
+        <h2>Contact Details</h2>
+        <p>
+            For further information about the University of Edinburgh Art Collections please contact:
+        </p>
+        <p><strong>Neil Lebeter</strong><br />
+            Art Collections Curator<br />
+            University of Edinburgh,<br />
+            Main Library,<br />
+            30 George Square,<br />
+            Edinburgh,<br />
+            EH8 9LJ<br />
+            <a class="email" href="mailto:neil.lebeter@ed.ac.uk">neil.lebeter@ed.ac.uk</a><br />
+            <br />
+            <a href="https://twitter.com/UoEArtArchives" title="Art Collections on Twitter">Art Collections on Twitter</a><br />
+            <a href="http://uoeartandarchives.tumblr.com/" title="Art Collections on Tumblr">Art Collections on Tumblr</a><br />
+
+    </div>
+</div>
