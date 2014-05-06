2f7e5bc2017a6d80a9eb4ba02e433bb7fd995d1d
Ianthe Sutherland <ianthe88@gmail.com>
2014-05-06 10:16:57 +0100

    LIB001-683: Addition of calendars prefix and removal of hardcoded configs and themes on collections pages

--- config/skylight.php
+++ config/skylight.php
@@ -1,7 +1,7 @@
 <?php
 
     // Uncomment this if you want to use urls of the form http://.../prefix/...
-    $config['skylight_url_prefixes'] = array('physics', 'mimed', 'art');
+    $config['skylight_url_prefixes'] = array('physics', 'mimed', 'art', 'calendars');
 
     // The URL of the parent solr server
     $config['skylight_solrbase'] = 'http://localhost:8080/solr/search/';
