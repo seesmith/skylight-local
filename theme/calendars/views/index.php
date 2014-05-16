b2c46d0517380c276e346ae352eeef9c6847a72e
Claire Knowles <claire.knowles@ed.ac.uk>
2014-05-16 14:10:07 +0100

    LIB001-708 - Calendars Index page updated to represent the 2014 calendar

---
+++ theme/calendars/views/index.php
@@ -0,0 +1,21 @@
+<div class="record">
+    <div class="content byEditor">
+        <h1>Centre for Research Collections Calendar 2014 - Bygone Edinburgh</h1>
+
+        <div id="tile-block">
+            <a href="./record/21816" alt="Curling at Blackford Pond" title="January, Curling at Blackford Pond"><div id="tile-1" class="art-tile"></div></a>
+            <a href="./record/21817" alt="Anatomy Museum in the University of Edinburgh Medical School" title="February, Anatomy Museum in the University of Edinburgh Medical School"><div id="tile-2" class="art-tile"></div></a>
+            <a href="./record/21818" alt="Demonstration of the Cantilever Principle of the Forth Bridge" title="March, Demonstration of the Cantilever Principle of the Forth Bridge"><div id="tile-3" class="art-tile"></div></a>
+            <a href="./record/21819" alt="Edinburgh from Calton Hill" title="April, Edinburgh from Calton Hill"><div id="tile-4" class="art-tile"></div></a>
+            <a href="./record/21820" alt="Newhaven Fishermen" title="May, Newhaven Fishermen"><div id="tile-5" class="art-tile"></div></a>
+            <a href="./record/20412" alt="Children in Tron Square, Edinburgh" title="June, Children in Tron Square, Edinburgh"><div id="tile-6" class="art-tile"></div></a>
+            <a href="./record/20416" alt="The High Street with John Knox's House" title="July, The High Street with John Knox's House"><div id="tile-7" class="art-tile"></div></a>
+            <a href="./record/19807" alt="The Forth Bridge under Construction" title="August, The Forth Bridge under Construction"><div id="tile-8" class="art-tile"></div></a>
+            <a href="./record/20418" alt="Boys at Newhaven, Edinburgh" title="September, Boys at Newhaven, Edinburgh"><div id="tile-9" class="art-tile"></div></a>
+            <a href="./record/19578" alt="Trinity College Chapel and Hospital, with Calton Hill in the background, Edinburgh" title="October, Trinity College Chapel and Hospital, with Calton Hill in the background, Edinburgh"><div id="tile-10" class="art-tile"></div></a>
+            <a href="./record/20412" alt="Old College Quadrangle, University of Edinburghh" title="November, Old College Quadrangle, University of Edinburgh"><div id="tile-11" class="art-tile"></div></a>
+            <a href="./record/19496" alt="Skating on Duddingston Loch" title="December, Skating on Duddingston Loch"><div id="tile-12" class="art-tile"></div></a>
+        </div>
+
+    </div>
+</div>
