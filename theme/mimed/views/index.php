6d104ee08ddf97b6352d553f1a012c1c34805fad
Ianthe Sutherland <ianthe88@gmail.com>
2014-05-20 15:21:42 +0100

    LIB001-714: Addition of carousel on MIMEd homepage

---
+++ theme/mimed/views/index.php
@@ -0,0 +1,89 @@
+<div class="content">
+    <div class="content byEditor">
+        <p>St Cecilia’s Hall is home to the collections of early keyboard and plucked strings, part of Edinburgh University
+            Collection of Historic Musical Instruments, which ranks among the world's most important
+            collections of musical heritage. The sister collection is currently on display and in store at the Reid Concert
+            Hall Museum of Instruments. As part of the Redevelopment Project the two collections will be brought together as one,
+            creating a single visitor destination at St Cecilia’s Hall. It has official Recognised Collection of National
+            Significance to Scotland status. </p>
+
+        <div class="jcarousel-wrapper">
+            <div class="jcarousel" data-jcarousel="true">
+                <ul style="left: 0px; top: 0px;">
+                    <li><a href="./record/16327">
+                        <img alt="Triple Manual Harpsichord" title="Triple Manual Harpsichord" src="<?php echo base_url(); ?>theme/mimed/images/carousel/triple-manual-harpsichord.jpg">
+                    </a></li>
+                    <li><a href="./record/17674">
+                        <img alt="Bass Saxophone" title="Bass Saxophone" src="<?php echo base_url(); ?>theme/mimed/images/carousel/bass-saxophone.jpg">
+                    </a></li>
+                    <li><a href="./record/16782">
+                        <img alt="Kettle Drum" title="Kettle Drum" src="<?php echo base_url(); ?>theme/mimed/images/carousel/kettle-drum.jpg">
+                    </a></li>
+                    <li><a href="./record/14715">
+                        <img alt="Viola D&#39;Amore" title="Viola D&#39;Amore" src="<?php echo base_url(); ?>theme/mimed/images/carousel/viola-damore.jpg">
+                    </a></li>
+                    <li><a href="./record/17041">
+                        <img alt="Tam-Tam" title="Tam-Tam" src="<?php echo base_url(); ?>theme/mimed/images/carousel/tam-tam.jpg">
+                    </a></li>
+                    <li><a href="./record/14715">
+                        <img alt="Upright Pianoforte" title="Upright Pianoforte" src="<?php echo base_url(); ?>theme/mimed/images/carousel/upright-pianoforte.jpg">
+                    </a></li>
+                    <li><a href="./record/17987">
+                        <img alt="Shawm" title="Shawm" src="<?php echo base_url(); ?>theme/mimed/images/carousel/shawm.jpg">
+                    </a></li>
+                    <li><a href="./record/16820">
+                        <img alt="Panpipes" title="Panpipes" src="<?php echo base_url(); ?>theme/mimed/images/carousel/panpipes.jpg">
+                    </a></li>
+                    <li><a href="./record/17120">
+                        <img alt="Sousaphone" title="Sousaphone" src="<?php echo base_url(); ?>theme/mimed/images/carousel/sousaphone.jpg">
+                    </a></li>
+                    <li><a href="./record/14923">
+                        <img alt="Banjo" title="Banjo" src="<?php echo base_url(); ?>theme/mimed/images/carousel/banjo.jpg">
+                    </a></li>
+                    <li><a href="./record/15548">
+                        <img alt="Clarinet" title="Clarinet" src="<?php echo base_url(); ?>theme/mimed/images/carousel/clarinet.jpg">
+                    </a></li>
+                    <li><a href="./record/16671">
+                        <img alt="Bow Harp" title="Bow Harp" src="<?php echo base_url(); ?>theme/mimed/images/carousel/bow-harp.jpg">
+                    </a></li>
+                    <li><a href="./record/15002">
+                        <img alt="Bassoon" title="Bassoon" src="<?php echo base_url(); ?>theme/mimed/images/carousel/bassoon.jpg">
+                    </a></li>
+                    <li><a href="./record/16112">
+                        <img alt="Double-Belled Euphonium" title="Double-Belled Euphonium" src="<?php echo base_url(); ?>theme/mimed/images/carousel/double-belled-euphonium.jpg">
+                    </a></li>
+                    <li><a href="./record/16089">
+                        <img alt="Guitar" title="Guitar" src="<?php echo base_url(); ?>theme/mimed/images/carousel/guitar.jpg">
+                    </a></li>
+                    <li><a href="./record/17775">
+                       <img alt="Stock and Horn" title="Stock and Horn" src="<?php echo base_url(); ?>theme/mimed/images/carousel/stock-and-horn.jpg">
+                    </a></li>
+                    <li><a href="./record/14592">
+                       <img alt="Xylophone" title="Xylophone" src="<?php echo base_url(); ?>theme/mimed/images/carousel/xylophone.jpg">
+                    </a></li>
+                </ul>
+            </div>
+
+            <a class="jcarousel-control-prev" href="#" data-jcarouselcontrol="true">‹</a>
+            <a class="jcarousel-control-next" href="#" data-jcarouselcontrol="true">›</a>
+
+        </div>
+
+        <p>The emphasis of the Collection is on instruments that are no longer in regular current use and the collecting
+            policy is to acquire instruments when they fall out of use rather than to collect instruments by contemporary
+            makers. The Collection thus covers the period from the 16th century (the earliest from which examples are
+            available for acquisition) to the 20th century (the most recent from which instruments can be regarded as
+            historic).</p>
+        <p>Many of the instruments are still playable and through an established concert programme and as a regular
+            venue during the Edinburgh International Festival, the Concert Room provides a contemporaneous setting for
+            performances, within which the audience can be seen as the interface between the University and the public.
+            For instance, St Cecilia’s Hall is the only place in the world where it is possible to hear 18th century music
+            in an 18th century concert hall played on 18th century instruments.</p>
+
+        <p>The instruments are supplemented by an archive of original materials, working
+            papers and a sound archive. The Collection as a whole attracts researchers from far and wide and is an
+            extensively cited resource in international scholarship. Instruments are lent to prestigious exhibitions at home
+            and internationally. </p>
+    </div>
+
+</div>
