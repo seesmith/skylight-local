9e414b6d3f61acc31bcbeae6d01fd0d8ca2cf8df
Claire Knowles <claire.knowles@ed.ac.uk>
2014-05-22 13:30:12 +0100

    LIB001-729 - Review and update static pages in Art. CLDS, Calendars and MIMED

--- static/calendars/about.php
+++ static/calendars/about.php
@@ -1,20 +1,39 @@
 <div class="content">
-    <div class="content byEditor">
-        <h1>About the University of Edinburgh Art Collection</h1>
-        <p>
-        </p>
+    <div class="content byEditor about">
+        <h1>About the University of Edinburgh Calendar Collection</h1>
+        <p>Calendars are produced every year to showcase the diverse range of material held in the University's collections. These are generally produced around a specific theme, e.g. Bygone Edinburgh (2014), Historic Musical Instruments (2012) and Travel (2008)  but in some years (e.g. 2010 and 2011) a selection of images illustrating Treasures of the Collections are selected.</p>
+        <p>All images were created by the Digital Imaging Unit and are copyright the University of Edinburgh.<p/>
+        <p>Images from all calendars, from 2006 onwards will be available in this collection.<p/>
+        <ul>
+            <li>2006 - Treasures from the Collection</li>
+            <li>2007 - The Natural World</li>
+            <li>2008 - Travel</li>
+            <li>2009 - no calendar</li>
+            <li>2010 - Treasures from Special Collections</li>
+            <li>2011 - Treasures from University Collections</li>
+            <li>2012 - Historic Musical Instruments at St. Cecilia's Hall</li>
+            <li>2013 - Japanese Paintings of Children's Games by Kobayashi Eitaku</li>
+            <li>2014 - Bygone Edinburgh</li>
+        </ul>
 
-        <h1>Art Collections Online</h1>
-        <p>
         </p>
         <h1>University Collections</h1>
         <p>
-            Info about the University Collections link to collections.ed.ac.uk
+            Special Collections include manuscripts and archives, rare books, photographs, printed and handwritten music, architectural drawings, theses and microfilms. The collections relate to University of Edinburgh and its history, and to Scotland and the wider world.</p>
+        <p>In total we have about 35 kilometres of historic material, much of which cannot be found anywhere else in the world. <a href="http://collections.ed.ac.uk" Title="University Collections Home">collections.ed.ac.uk</a>
         </p>
 
-        <h1>Contact Details</h1>
+        <h1>Contact</h1>
         <p>
-            For further information about the University of Edinburgh Digital Imaging Unit please contact:
+            Centre for Research Collections<br />
+            Edinburgh University Library,<br />
+            George Square,<br />
+            Edinburgh,<br />
+            EH8 9LJ<br />
+            Tel:: +44(0)131 650 8379<br />
+            Fax: +44(0)131 650 2922<br />
+            <a class="email" href="mailto:is-crc@ed.ac.uk">is-crc@ed.ac.uk</a><br />
+
         </p>
 
     </div>
--- static/calendars/licensing.php
+++ static/calendars/licensing.php
@@ -2,7 +2,7 @@
     <div class="content byEditor">
         <h1>Licensing and Copyright Information</h1>
         <p>
-            We endeavour to undertake reasonable efforts to make sure that the reproduction of content on this site is done with the consent of copyright owners. However, due to the size of the collection, this has not been possible in all cases and some clearances are still awaiting action. All material that is reproduced here is only done so at low resolution and for the purposes of teaching and research.
+        All images within this collection are copyright the University of Edinburgh.
         </p>
         <p>
         If you are a rights holder and wish to request the removal of a work to which you hold the rights (or is not covered by a copyright exception under the UK Copyright, Designs and Patents Act 1988) please contact us in writing stating the following:
