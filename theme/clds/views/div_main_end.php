ed86870657b0cd73a0ba761240dc56a2071cf6b9
Ianthe Sutherland <ianthe88@gmail.com>
2014-05-23 11:38:48 +0100

    LIB001-740: Setting images and exhibitions to open in new window

--- theme/clds/views/div_main_end.php
+++ theme/clds/views/div_main_end.php
@@ -3,8 +3,8 @@
                 <a href="./" title="University of Edinburgh Collections" class="collections">Home</a>
                 <a href="<?php echo base_url(); ?>art" title="University of Edinburgh Art Collection" class="art">Art</a>
                 <a href="<?php echo base_url(); ?>mimed" title="University of Edinburgh Musical Instrument Museums" class="mimed">Mimed</a>
-                <a href="http://exhibitions.ed.ac.uk/" title="University of Edinburgh Exhibitions" class="exhibitions">Exhibitions</a>
-                <a href="http://images.is.ed.ac.uk/" title="University of Edinburgh Image Collections" class="images">Images</a>
+                <a href="http://exhibitions.ed.ac.uk/" title="University of Edinburgh Exhibitions" class="exhibitions" target="_blank">Exhibitions</a>
+                <a href="http://images.is.ed.ac.uk/" title="University of Edinburgh Image Collections" class="images" target="_blank">Images</a>
                 <a href="<?php echo base_url(); ?>calendars" title="University of Edinburgh Calendars" class="calendars last">Calendars</a>
             </div>
             <div class="footer-disclaimer">
