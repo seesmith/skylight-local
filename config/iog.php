3aec5a54f55e4fd7102217941bb3c5710f2cacf3
Claire Knowles <claire.knowles@ed.ac.uk>
2014-06-26 09:25:14 +0100

    LIB001-788 iog config and css

---
+++ config/iog.php
@@ -0,0 +1,90 @@
+<?php
+
+$config['skylight_appname'] = 'iog';
+
+// Uncomment this if you are using a url of the form http://.../art/...
+$config['skylight_url_prefix'] = 'iog';
+
+$config['skylight_theme'] = 'iog';
+
+$config['skylight_fullname'] = 'Scottish Government Yearbooks';
+
+$config['skylight_adminemail'] = 'lddt@mlist.is.ed.ac.uk';
+
+$config['skylight_oaipmhcollection'] = 'hdl_10683_4';
+
+
+// Container ID and the field used in solr index to store this ID. Used for restricting search/browse scope.
+$config['skylight_container_id'] = '15';
+$config['skylight_container_field'] = 'location.coll';
+
+$config['skylight_fields'] = array('Title' => 'dc.title.en',
+    'Author' => 'dc.contributor.author.en',
+    'Subject' => 'dc.subject.en',
+    'Type' => 'dc.type.en',
+    'Abstract' => 'dc.description.abstract',
+    'Date' => 'dc.date.issued',
+    'Accession Date' => 'dc.date.accessioned_dt',
+    'Number of Pages' => 'dc.coverage.spatial',
+    'Citation' => 'dc.identifier.citation.en',
+    'ISBN' => 'dc.identifier.isbn',
+    'Page Numbers' => 'dc.format.extent',
+    'Publisher' => 'dc.publisher.en',
+    'Series' => 'dc.relation.ispartofseries.en',
+);
+
+$config['skylight_date_filters'] = array('Date' => 'dateIssued.year_sort');
+$config['skylight_filters'] = array('Author' => 'author_filter', 'Subject' => 'subject_filter');
+$config['skylight_filter_delimiter'] = ':';
+
+$config['skylight_meta_fields'] = array('Title' => 'dc.title.en',
+    'Author' => 'dc.creator',
+    'Abstract' => 'dc.description.abstract',
+    'Subject' => 'dc.subject',
+    'Date' => 'dc.date.issued',
+    'Type' => 'dc.type');
+
+$config['skylight_recorddisplay'] = array('Title','Author','Subject','Citation','Series','Date','Page Numbers','Number of Pages','Publisher','ISBN','Type','Abstract');
+
+$config['skylight_searchresult_display'] = array('Title','Author','Subject','Type','Abstract');
+
+$config['skylight_search_fields'] = array('Keywords' => 'text',
+    'Subject' => 'dc.subject',
+    'Type' => 'dc.type',
+    'Author' => 'dc.creator'
+);
+
+$config['skylight_sort_fields'] = array('Title' => 'dc.title',
+    'Date' => 'dc.date.issued_dt',
+    'Author' => 'dc.creator'
+);
+
+$config['skylight_feed_fields'] = array('Title' => 'Title',
+    'Author' => 'Author',
+    'Subject' => 'Subject',
+    'Description' => 'Abstract',
+    'Date' => 'Date');
+
+$config['skylight_results_per_page'] = 10;
+$config['skylight_share_buttons'] = false;
+
+$config['skylight_homepage_recentitems'] = false;
+
+// Set to the number of minutes to cache pages for. Set to false for no caching.
+// This overrides the setting in skylight.php so is commented by Demo
+$config['skylight_cache'] = false;
+
+// Digital object management
+$config['skylight_display_thumbnail'] = true;
+$config['skylight_link_bitstream'] = true;
+
+// Display common image formats in "light box" gallery?
+$config['skylight_lightbox'] = true;
+$config['skylight_lightbox_mimes'] = array('image/jpeg', 'image/gif', 'image/png');
+
+// Language and locale settings
+$config['skylight_language_default'] = 'en';
+$config['skylight_language_options'] = array('en', 'ko', 'jp');
+
+
+?>
