af957d90aca9866bd43bc9ec9ee02bdc2de3db24
Claire Knowles <claire.knowles@ed.ac.uk>
2014-07-20 01:21:54 +0100

    LIB001-788 iog config and css

--- static/iog/about.php
+++ static/iog/about.php
@@ -7,6 +7,33 @@
         <p>
 
         </p>
+        <h1>Image Credits</h1>
+        <p>
+            <ul>
+            <li><a href="https://www.flickr.com/photos/chris-yunker/2504698578" target="_blank">Devolution</a> CC-BY-SA 2.0 Chris Yunker
+            </li>
+            <li><a href="https://www.flickr.com/photos/marian_ey/3447551050" target="_blank">Elections</a> CC-BY-NC 2.0 marian_ey
+            </li>
+            <li><a href="http://images.is.ed.ac.uk/luna/servlet/s/a6p4ip" target="_blank">Gender</a>
+                &copy; The University of Edinburgh
+            </li>
+            <li><a href="http://images.is.ed.ac.uk/luna/servlet/s/p30b33" target="_blank">Health</a>
+                &copy; The University of Edinburgh
+            </li>
+            <li><a href="http://images.is.ed.ac.uk/luna/servlet/s/x924zp" target="_blank">Highlands</a>
+                &copy; The University of Edinburgh
+            </li>
+            <li><a href="http://collections.ed.ac.uk/record/697/1/ol00044.jpg  target="_blank"">Local Government</a>
+                &copy; The University of Edinburgh
+            </li>
+            <li><a href="https://www.flickr.com/photos/noodlepie/1037285312" target="_blank">Media</a> CC-BY-NC-SA 2.0 Graham Holliday
+            </li>
+            <li><a href="https://www.flickr.com/photos/conner395/4917919154" target="_blank">Nationalism</a> CC-BY 2.0 Dave Conner
+            </li>
+            <li><a href="http://images.is.ed.ac.uk/luna/servlet/s/2ot58r" target="_blank">Religion</a> &copy; The University of Edinburgh
+            </li>
+            </ul>
+        </p>
 
     </div>
 </div>
