5cd54d2e752226c2183438491dab9481ecf33b14
Claire Knowles <claire.knowles@ed.ac.uk>
2014-07-29 13:16:47 +0100

    LIB001-788 iog config and css

--- theme/iog/views/index.php
+++ theme/iog/views/index.php
@@ -1,6 +1,7 @@
 <div class="content">
     <div class="content byEditor">
-        <p>Intro blurb. </p>
+        <p><h6>Welcome to the digital archive of the <span class="italic">Scottish Government Yearbooks</span>, published by the University of Edinburgh's
+            'Unit for the Study of Government in Scotland' between 1976 and 1992. </h6></p>
 
         <div class="jcarousel-wrapper">
             <div class="jcarousel" data-jcarousel="true">
@@ -62,12 +63,22 @@
                 </ul>
             </div>
 
+
             <a class="jcarousel-control-prev" href="#" data-jcarouselcontrol="true">‹</a>
             <a class="jcarousel-control-next" href="#" data-jcarouselcontrol="true">›</a>
 
         </div>
-
-        <p>Blurb part two </p>
+        <p>The Unit was set up in the mid-1970s
+            to address 'Our Changing Scotland' and the certainty – as it seemed at the time – of a devolved Assembly in
+            Edinburgh. The <span class="italic">Yearbooks</span> reflect the spirit of that enterprise: contributions cut
+            across academic disciplines … sociology, political science, social policy, history, economics, theology, education, medicine, law.
+            And they spanned a wide range of academic institutions, centred upon – but not limited to – Scotland’s universities.
+            But the Yearbooks went beyond academia to embrace lawyers, journalists, political activists, policy makers,
+            civil servants, public sector managers, local government officers and clerics. Through this rich mix the
+            <span class="italic">Yearbooks</span> sought, as the opening volume’s <a
+                href="https://www.era.lib.ed.ac.uk/bitstream/1842/8996/1/1976_1_Our%20changing%20Scotland.pdf" target="_blank">editorial</a> made clear, to engage with a Scotland which was proving
+            to be "a perplexing place".
+        </p>
     </div>
 
 </div>
