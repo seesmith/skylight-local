334019102ffda35f0b1494a4bb4a31769a546f8f
Claire Knowles <claire.knowles@ed.ac.uk>
2014-08-12 15:54:14 +0100

    LIB001-843 Add 2015 Calendar Metadata and Images

---
+++ static/calendars/laing.php
@@ -0,0 +1,116 @@
+<div class="content">
+    <div class="content byEditor">
+        <h1>The Collection</h1>
+
+        <p>
+            Still largely unexplored, the David Laing collection is one of the most rich and complex collections of its
+            kind. David Laing was the son of an Edinburgh bookseller and one of the greatest collectors of the culture
+            and history of Scotland and the world. On his death in 1878, his extensive collection of manuscripts was
+            bequeathed to the University of Edinburgh where they now form the centrepiece of the Library’s collection of
+            rare books and archives.
+        </p>
+        <p>
+            During his lifetime, David Laing sought out imperilled documents from across Europe, even saving precious
+            papers from waste collections. In doing so he accumulated a vast amount of material including original poems
+            by Robert Burns, 103 Western medieval manuscripts, 14th century Hebrew manuscripts on vellum and thousands
+            of letters from the 16th to the 19th centuries. A description of him by John L. Weir states ‘we are told
+            when he visited any bookseller’s shop he at once took off his coat and ransacked the place from floor to
+            ceiling’.
+        </p>
+        <p>
+            Since the time of the bequest, a number of stunning items from the collection have become
+            internationally-known, such as the Van Meer album of paintings which includes one of the earliest paintings
+            of a Native American. Items more recently uncovered in the last few years include 9th-century fragments from
+            an Egyptian Koran; documents relating to Flodden, Covenanters and the Jacobite rebellions; and letters
+            signed by Simon Bolivar, ‘the Liberator’ of South America.
+        </p>
+        <p>
+            There are approximately half a million individual documents in the collection, many of which are currently
+            difficult to access for a number of reasons. As time has passed the condition of these fragile items has
+            deteriorated, which makes them hard to handle safely. In addition, the absence of a full catalogue of the
+            collection means that much of its content remains largely unknown.
+        </p>
+        <p>
+            We believe that many more discoveries will be made once we can apply modern archival and preservation
+            methods to make the papers available to a world-wide audience.
+        </p>
+
+        <h1>Preserving the collection</h1>
+
+        <p>
+            The University has already invested significantly in this collection to ensure its preservation. It is now
+            housed in secure, environmentally-controlled conditions in the Centre for Research Collections. A recent
+            professional conservation survey has scoped out the main issues facing the collection and we would like to
+            embark on a project to test our approach to a collection of this size and importance. This will become the
+            forerunner to a larger project that will allow us to reach the dark heart of this enormous collection and to
+            open up paths for future researchers.
+        </p>
+        <p>
+            We now to need to raise funds in order to: </p>
+        <ul>
+            <li> Create online collection-level records for all the main components of the collection and identify
+                priority materials for in-depth cataloguing in a longer-term project
+            </li>
+            <li> Rehouse the most vulnerable and unprotected items using archival conservation materials, following the
+                recommendations of the recent conservation survey
+            </li>
+            <li> Digitise a range of materials from across the collection and make these available via our Images
+                website and a range of social media, to explore the issues and opportunities in such a diverse
+                collection
+            </li>
+            <li> Establish a supporting group of expert scholars to promote research and teaching using newly-discovered
+                items from the collection, who would also help identify items suitable for text-encoding and
+                crowd-sourcing to attract wider interest
+            </li>
+            <li> Through discussions between scholars and curators, fully establish the overall architecture of the
+                collection, the provenance of and relationships between its components, so that future cataloguing and
+                organisation rests on a sound basis
+            </li>
+            <li> Create a PhD studentship earmarked for new research based on the collection</li>
+            <li> Build momentum and support to establish a major project to catalogue, conserve and digitise – as far as
+                possible – the whole collection
+            </li>
+        </ul>
+
+        <h1>Our Vision</h1>
+
+        <p>
+            This project will transform not only the Laing collection itself – it would also elevate the status of the
+            Centre for Research Collections amongst the world’s leading research destinations and cement the University
+            of Edinburgh’s reputation as a magnet for researchers, students and visitors from all over the world.
+        </p>
+        <p>
+            The project will culminate in the creation of a digital portal allowing people to navigate through the
+            diverse parts of the collection. For some sections there will be detailed entries for individual items (e.g.
+            the medieval manuscript Books of Hours, with images of their vibrant colour illuminations). For other
+            sections there will be digital images with text encoding, transcriptions and translations (e.g. the early
+            Scots poetry) – creating opportunities for data mining and other kinds of sophisticated research into the
+            vast quantity of material. All this will reveal the potential for a longer-term project to make this raw
+            source material fully intelligible and attractive to students, researchers and people across the world.
+        </p>
+        <p>All of this work will be informed by the close academic partnerships that have secured our previous
+            successes, ensuring that specific expertise will be on hand to deal with the extreme diversity of material
+            within the collection. In the project team, archivists, scholars, photographers and conservators will work
+            together to bring the collection to life. This approach will, ultimately, transform access to other
+            collections which present similar challenges, and will lead to collaborations with other galleries, museums
+            and libraries.
+        </p>
+        <p>
+            David Laing was at the cutting edge of scholarly collecting in the 19th century. The incredible results of
+            his labours likewise demand the best combination of 21st century technology, skills and knowledge to make
+            these treasures fully accessible at last.
+        </p>
+        <p>
+            <h5>If you would like to talk to us about supporting the Laing Collection project, please contact:</h5>
+            Development Officer<br/>
+            Libraries, Museums and Galleries,<br/>
+            Development and Alumni<br/>
+            Charles Stewart House<br/>
+            9 - 16 Chambers Street<br/>
+            Edinburgh <br/>
+            EH1 1HT<br/>
+            <a mail="laingcollection@ed.ac.uk">laingcollection@ed.ac.uk</a></a><br/>
+        </p>
+        <a href="./search/*:*/Subject:%22images+from+the+david+laing+collection+2015%7C%7C%7CImages+from+the+David+Laing+Collection+2015%22">Images from the Laing Collection</a>
+    </div>
+</div>
