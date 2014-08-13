726dc52bcfe9b9c3fafd9181f5e8a724656cddda
Scott Renton <scott.renton@ed.ac.uk>
2014-08-13 09:40:10 +0100

    LIB001-837 Exhibitions static pages

---
+++ static/exhibitions/50years.php
@@ -0,0 +1,27 @@
+<div class="content">
+    <div class="content byEditor">
+        <a href ="./"><h1>Fifty Years, Fifty Books</h1></a>
+        <div id="exhib-page-big-block">
+            <div id="exhib-tile-5" class="art-tile"></div>
+        </div>
+        <h2>About the Exhibition</h2>
+        <h3>Opening: 28th March 2014 | Where: Exhibition Gallery, Main Library, George Square | Closing: 14th June 2014 | Curated by: Emma Smith</h3>
+
+        <p>
+            Fifty Years, Fifty Books: Purchases by the Friends of Edinburgh University Library, 1962-2012
+        </p>
+        <p>
+            In 1962 the Friends of Edinburgh University Library held their first meeting.  They agreed to work to support “the purchase of rare or valuable books, manuscripts or documents” and “to enhance the resources and reputation of the library”.  Fifty years later, we want to recognise the great contribution made by the Friends.  Thanks to their consistent support, our Special Collections are now among the world’s foremost for research, teaching and scholarship.  The items on display here represent a small selection of the wide range of material purchased with the help of the Friends.
+        </p>
+        <p>
+            Back in 1962, the Library was based in the early 19th century buildings at Old College.  Today, collections are stored in purpose-built strongrooms and are made available through digital media as well as through the reading rooms situated on the 6th floor.  The Friends continue to help us add to the collections which represent some of the University’s greatest assets – both now and in centuries to come.
+        </p>
+        <p>
+            Many of these items have never been seen on public display before.  This Exhibition Gallery – opened in 2009 – was itself supported by the generosity of the Friends
+        </p>
+        <h2> Further Resources</h2>
+        <p>
+            <a href = "http://university-of-edinburgh-crc-images.culturalspot.org/home" target = "_blank" title = "Fifty Years, Fifty Books at Google" alt = "Fifty Years, Fifty Books at Google"> Fifty Years, Fifty Books Exhibition</a> at Google Open Gallery.</p>
+        </p>
+    </div>
+</div>
