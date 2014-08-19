2f35b516515a5bc83fbbdfcfb917cdfcd604ea15
Claire Knowles <claire.knowles@ed.ac.uk>
2014-08-19 13:50:32 +0100

    LIB001-877 - IOG Blog link

--- theme/iog/views/header.php
+++ theme/iog/views/header.php
@@ -97,7 +97,7 @@
                 <li><a href="http://www.ed.ac.uk"  target="_blank" title="University of Edinburgh Home">University of Edinburgh</a></li>
             </ul>
             <ul class="menu-links">
-                <li><a href="" title="Blog" target="_blank">Blog</a></li>
+                <li><a href="http://scottishgovernmentyearbooks.wordpress.com" title="Blog" target="_blank">Blog</a></li>
                 <li><a href=" http://www.era.lib.ed.ac.uk/" title="Edinburgh Research Archive" target="_blank">ERA</a></li>
                 <li><a href="http://www.institute-of-governance.org" title="Institute of Governance Home" target="_blank">IOG</a></li>
                 <li><a href=" http://www.sps.ed.ac.uk/" title="School of Social and Political Science" target="_blank">SPS</a></li>
