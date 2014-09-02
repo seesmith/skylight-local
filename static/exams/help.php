b61be0cb63d029b33489fe44d60bec4cf1e7e82d
Ianthe Sutherland <ianthe88@gmail.com>
2014-09-02 13:16:27 +0100

    LIB001-902: Fixes for accessibility

--- static/exams/help.php
+++ static/exams/help.php
@@ -2,5 +2,8 @@
    <h1>Exam Papers Online Help</h1>
 
    <p>Please contact <a href="mailto:exam.papers@ed.ac.uk" title="Email exampapers@ed">exam.papers@ed.ac.uk</a> with any queries concerning past exam papers or use the <a href="./feedback/" title="Feedback Form Link">Feedback form</a> for comments or suggestions on the web pages.</p>
-   <br />
+   <hr />
+   <p>All exam paper downloads on this site are in PDF format. Your browser should automatically display the PDF but if this is not possible, there will be a link for downloading the PDF. If you do not have a PDF reader installed, download Adobe Acrobat Reader <a href="http://get.adobe.com/uk/reader/" title="Link to download and install Adobe Acrobat PDF Reader">here</a>.</p>
+   <hr />
+   <p>Users who wish to use assisted software are advised to use Internet Explorer.</p>
 </div>
