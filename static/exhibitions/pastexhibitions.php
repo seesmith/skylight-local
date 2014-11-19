e088e66397de91250a7e27aca7d79b0297d8ca99
Claire Knowles <claire.knowles@ed.ac.uk>
2014-11-19 15:40:15 +0000

    LIB001-998 Add Emma Gillies Information to Exhibitions and update index page LIB001-1008 Change 300 chemistry logo on home page

---
+++ static/exhibitions/pastexhibitions.php
@@ -0,0 +1,33 @@
+<div class="content">
+    <div class="content byEditor">
+
+        <h1>Past Exhibitions:</h1>
+        <ul>
+            <li>
+                <h3><a href="./rashid" alt="Rashid al-Din" title="Rashid al-Din Information Page">The World History of Rashid al-Din, 1314</a></h3>
+                <p>2 Aug - 31 Oct 2014</p>
+                <a href="./rashid" alt="Rashid al-Din" title="Rashid al-Din"><div id="exhib-tile-1" class="art-tile"></div></a>
+            </li>
+            <li>
+                <h3><a href="./50years" alt="50 Years, 50 Books" title="50 Years, 50 Books Information Page">50 Years, 50 Books</a></h3>
+                <p>28 Mar - 14 Jun 2014</p>
+                <a href="./50years" alt="50 Years, 50 Books" title="50 Years, 50 Books"><div id="exhib-tile-2" class="art-tile"></div></a>
+            </li>
+            <li>
+                <h3><a href="./collected" alt="Collect.ed" title="Collect.ed Information Page">Collect.ed</a></h3>
+                <p>6 Dec 2013 - 1 Mar 2014</p>
+                <a href="./collected" alt="Collect.ed" title="Collect.ed"><div id="exhib-tile-3" class="art-tile"></div></a>
+            </li>
+            <li>
+                <h3><a href="./cradle" alt="Cradle of Chemistry" title="Cradle of Chemistry Information Page">Cradle of Chemistry</a></h3>
+                <p>1 Aug 2013 - 2 Nov 2013</p>
+                <a href="./cradle" alt="Cradle of Chemistry" title="Cradle of Chemistry"><div id="exhib-tile-4" class="art-tile"></div></a>
+            </li>
+            <li>
+                <h3><a href="./objectlessons" alt="Object Lessons" title="Object LessonsInformation Page">Object Lessons</a></h3>
+                <p>2003</p>
+                <a href="./objectlessons" alt="Object Lessons" title="Object Lessons"><div id="exhib-tile-5" class="art-tile"></div></a>
+            </li>
+        </ul>
+    </div>
+</div>
--- theme/exhibitions/views/index.php
+++ theme/exhibitions/views/index.php
@@ -6,31 +6,29 @@
         <div id="current-banner" class="art-tile"></div>
         </div>
         <div id="past-exhib-block">
-            <h1>Past Exhibitions:</h1>
-            <table>
-            <tr class="indextr">
-                <td class="ex-front-td">
-                    <h3><a href="./rashid" alt="Rashid al-Din" title="Rashid al-Din Information Page">The World History of Rashid al-Din, 1314</a></h3>
+            <h1><a href="./pastexhibitions" alt="Past Exhibitions" title="Past Exhibitions">Past Exhibitions</a>:</h1>
+                <ul class="ex-list">
+                    <li class="past-ex">
+                <h3><a href="./rashid" alt="Rashid al-Din" title="Rashid al-Din Information Page">The World History of Rashid al-Din, 1314</a></h3>
                     <p>2 Aug - 31 Oct 2014</p>
-                    <a href="./rashid" alt="Rashid al-Din" title="Rashid al-Din"><div id="exhib-tile-1" class="art-tile"></div></a>
-                </td>
-                <td class="ex-front-td">
+                    <a href="./rashid" alt="Rashid al-Din" title="Rashid al-Din"><div id="exhib-tile-1"></div></a>
+                </li>
+                    <li class="past-ex">
                     <h3><a href="./50years" alt="50 Years, 50 Books" title="50 Years, 50 Books Information Page">50 Years, 50 Books</a></h3>
                     <p>28 Mar - 14 Jun 2014</p>
-                    <a href="./50years" alt="50 Years, 50 Books" title="50 Years, 50 Books"><div id="exhib-tile-2" class="art-tile"></div></a>
-                </td>
-                <td class="ex-front-td">
+                    <a href="./50years" alt="50 Years, 50 Books" title="50 Years, 50 Books"><div id="exhib-tile-2"></div></a>
+                </li>
+                    <li class="past-ex">
                     <h3><a href="./collected" alt="Collect.ed" title="Collect.ed Information Page">Collect.ed</a></h3>
                     <p>6 Dec 2013 - 1 Mar 2014</p>
-                    <a href="./collected" alt="Collect.ed" title="Collect.ed"><div id="exhib-tile-3" class="art-tile"></div></a>
-                </td>
-                <td class="ex-front-td">
+                    <a href="./collected" alt="Collect.ed" title="Collect.ed"><div id="exhib-tile-3"></div></a>
+                </li>
+                    <li class="past-ex">
                     <h3><a href="./cradle" alt="Cradle of Chemistry" title="Cradle of Chemistry Information Page">Cradle of Chemistry</a></h3>
                     <p>1 Aug 2013 - 2 Nov 2013</p>
-                    <a href="./cradle" alt="Cradle of Chemistry" title="Cradle of Chemistry"><div id="exhib-tile-4" class="art-tile"></div></a>
-                </td>
-            </tr>
-            </table>
+                    <a href="./cradle" alt="Cradle of Chemistry" title="Cradle of Chemistry"><div id="exhib-tile-4"></div></a>
+                </li>
+            </ul>
         </div>
 
     </div>
