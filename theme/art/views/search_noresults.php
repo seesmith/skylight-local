59866f0c6533df697829ae2369672d2d595a43e5
Claire Knowles <claire.knowles@ed.ac.uk>
2015-02-24 10:25:53 +0000

    LIB001-1086 Improve Skylight integration with Google Analytics

---
+++ theme/art/views/search_noresults.php
@@ -0,0 +1,18 @@
+<div class="search_results">
+    <p>
+        There were no matching records. Please try again.
+    </p>
+</div>
+<script>
+    if (searchTerm != null)
+    {
+          ga('send', 'event', {
+            'eventCategory': 'Art',
+            'eventAction': 'Search',
+            'eventLabel': searchTerm,
+            'eventValue': 0
+
+        });
+    }
+
+</script>
