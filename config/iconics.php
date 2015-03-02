9c096db5192ae32a2f7292351cf8c526ef7b93f8
Claire Knowles <claire.knowles@ed.ac.uk>
2015-03-02 15:11:33 +0000

    New Iconics Site

---
+++ config/iconics.php
@@ -0,0 +1,102 @@
+<?php
+
+$config['skylight_appname'] = 'iconics';
+
+$config['skylight_theme'] = 'iconics';
+
+// Uncomment this if you are using a url of the form http://.../art/...
+$config['skylight_url_prefix'] = 'iconics';
+
+$config['skylight_fullname'] = 'Library and University Collections - Iconics';
+
+$config['skylight_adminemail'] = 'lddt@mlist.is.ed.ac.uk';
+
+$config['skylight_oaipmhcollection'] = 'hdl_10683_4';
+
+// set ga code
+if (strpos($_SERVER['HTTP_HOST'], "test") !== false) {
+    $config['skylight_ga_code'] = 'UA-25737241-6';
+}
+else {
+    $config['skylight_ga_code'] = 'UA-25737241-9';
+}
+
+
+
+// Container ID and the field used in solr index to store this ID. Used for restricting search/browse scope.
+$config['skylight_container_id'] = '31';
+$config['skylight_container_field'] = 'location.coll';
+
+$config['skylight_fields'] = array('Title' => 'dc.title.en',
+    'Author' => 'dc.contributor.author',
+    'Subject' => 'dc.subject.en',
+    'Type' => 'dc.type.en',
+    'Abstract' => 'dc.description.abstract.en',
+    'Date' => 'dc.date.issued_dt',
+    'Bitstream'=> 'dc.format.original.en',
+    'Thumbnail'=> 'dc.format.thumbnail.en',
+    'Description'=>'dc.description.en'
+);
+
+$config['skylight_date_filters'] = array('Date' => 'dateIssued.year_sort');
+$config['skylight_filters'] = array('Author' => 'author_filter', 'Subject' => 'subject_filter', 'Type' => 'type_filter');
+$config['skylight_filter_delimiter'] = ':';
+
+$config['skylight_meta_fields'] = array('Title' => 'dc.title.en',
+    'Author' => 'dc.contributor.author.en',
+    'Abstract' => 'dc.description.abstract.en',
+    'Subject' => 'dc.subject.en',
+    'Date' => 'dc.date.issued_dt',
+    'Type' => 'dc.type.en',
+    'Bitstream'=> 'dc.format.original',
+    'Thumbnail'=> 'dc.format.thumbnail'
+);
+
+$config['skylight_recorddisplay'] = array('Title','Author','Subject','Type','Abstract', 'Description');
+
+$config['skylight_searchresult_display'] = array('Title','Author','Subject','Type','Abstract');
+
+$config['skylight_search_fields'] = array('Keywords' => 'text',
+    'Subject' => 'dc.subject.en',
+    'Type' => 'dc.type.en',
+    'Author' => 'dc.contributor.author'
+);
+
+$config['skylight_sort_fields'] = array('Title' => 'dc.title',
+    'Date' => 'dc.date.issued_dt',
+    'Author' => 'dc.contributor.author'
+);
+
+$config['skylight_feed_fields'] = array('Title' => 'Title',
+    'Author' => 'Author',
+    'Subject' => 'Subject',
+    'Description' => 'Abstract',
+    'Date' => 'Date');
+
+$config['skylight_results_per_page'] = 10;
+$config['skylight_share_buttons'] = false;
+
+// $config['skylight_homepage_recentitems'] = false;
+
+// Set to the number of minutes to cache pages for. Set to false for no caching.
+// This overrides the setting in skylight.php so is commented by Demo
+$config['skylight_cache'] = false;
+
+// Digital object management
+$config['skylight_display_thumbnail'] = true;
+$config['skylight_link_bitstream'] = true;
+
+// Display common image formats in "light box" gallery?
+$config['skylight_lightbox'] = true;
+$config['skylight_lightbox_mimes'] = array('image/jpeg', 'image/gif', 'image/png');
+
+// Language and locale settings
+$config['skylight_language_default'] = 'en';
+$config['skylight_language_options'] = array('en', 'ko', 'jp');
+$config['skylight_highlight_fields'] = 'dc.title.en,dc.contributor.author,dc.subject.en,lido.country.en,dc.description.en,dc.relation.ispartof.en';
+
+$config['skylight_homepage_recentitems'] = false;
+$config['skylight_homepage_randomitems'] = true;
+$config['skylight_homepage_fullwidth'] = true;
+$config['skylight_search_header'] = false;
+?>
---
+++ theme/iconics/views/random_items.php
@@ -0,0 +1,181 @@
+
+ <div class="randoms">
+    <?php
+
+    $title_field = $this->skylight_utilities->getField('Title');
+    $author_field = $this->skylight_utilities->getField('Author');
+    $subject_field = $this->skylight_utilities->getField('Subject');
+    $abstract_field = $this->skylight_utilities->getField('Abstract');
+    $date_field = $this->skylight_utilities->getField('Date');
+    $type_field = $this->skylight_utilities->getField("Type");
+    $bitstream_field = $this->skylight_utilities->getField('Bitstream');
+    $thumbnail_field = $this->skylight_utilities->getField('Thumbnail');
+
+   //pop the first item from the list
+
+    $first_doc = array_shift($randomitems);
+
+
+    ?>
+        <!-- new layout -->
+        <div class="random-first">
+            <?php
+
+            $bitstream_array = array();
+
+            if(isset($first_doc[$bitstream_field])) {
+
+                $i = 0;
+                $j = 0;
+                $started = false;
+                // loop through to get min sequence
+                foreach ($first_doc[$bitstream_field] as $bitstream)
+                {
+                    $b_segments = explode("##", $bitstream);
+                    $b_filename = $b_segments[1];
+                    $b_seq = $b_segments[4];
+
+                    if((strpos($b_filename, ".jpg") > 0) || (strpos($b_filename, ".JPG") > 0)) {
+
+                        $bitstream_array[$b_seq] = $bitstream;
+
+                        if ($started) {
+                            if ($b_seq < $min_seq) {
+                                $min_seq = $b_seq;
+                            }
+                        }
+                        else {
+                            $min_seq = $b_seq;
+                            $started = true;
+                        }
+                    }
+
+                    $i++;
+
+                }
+
+                // if there is a thumbnail and a bitstream
+                if(isset($min_seq) && count($bitstream_array) > 0) {
+
+                    // get all the information
+                    $b_segments = explode("##", $bitstream_array[$min_seq]);
+                    $b_filename = $b_segments[1];
+                    $b_handle = $b_segments[3];
+                    $b_seq = $b_segments[4];
+                    $b_handle_id = preg_replace('/^.*\//', '',$b_handle);
+                    $b_uri = './record/'.$b_handle_id.'/'.$b_seq.'/'.$b_filename;
+
+                    $b_Link = '<a title = "' . $first_doc[$title_field][0] . '" class="fancybox" rel="group' . $j .'" href="' . $b_uri . '"> ';
+                    $b_Link .= '<img src = "'.$b_uri.'" class="random-first-image" title="'. $first_doc[$title_field][0] .'" /></a>';
+
+                    echo $b_Link;
+                }
+
+            } //end if there are bitstreams ?>
+            <div class = "random-iteminfo">
+                <h4><a href="./record/<?php echo $first_doc['id']?>"><?php echo $first_doc[$title_field][0]; ?></a></h4>
+                <?php
+                if (array_key_exists ($abstract_field, $first_doc)){
+                    echo $first_doc[$abstract_field][0];
+                }?>
+            </div>
+        </div>
+     <div class="random-list">
+    <ul class="random-listing">
+    <?php
+
+    foreach ($randomitems as $index => $doc) { ?>
+        <li>
+            <div class="random-itemdiv">
+
+                <div class = "random-thumbnail">
+
+                <?php
+
+                $bitstream_array = array();
+                if(isset($doc[$bitstream_field])) {
+
+                    $i = 0;
+                    $j = 0;
+                    $started = false;
+                    // loop through to get min sequence
+                    foreach ($doc[$bitstream_field] as $bitstream)
+                    {
+                        $b_segments = explode("##", $bitstream);
+                        $b_filename = $b_segments[1];
+                        $b_seq = $b_segments[4];
+
+                        if((strpos($b_filename, ".jpg") > 0) || (strpos($b_filename, ".JPG") > 0)) {
+
+                            $bitstream_array[$b_seq] = $bitstream;
+
+                            if ($started) {
+                                if ($b_seq < $min_seq) {
+                                    $min_seq = $b_seq;
+                                }
+                            }
+                            else {
+                                $min_seq = $b_seq;
+                                $started = true;
+                            }
+                        }
+
+                        $i++;
+
+                    }
+
+                    // if there is a thumbnail and a bitstream
+                    if(isset($min_seq) && count($bitstream_array) > 0) {
+
+                        // get all the information
+                        $b_segments = explode("##", $bitstream_array[$min_seq]);
+                        $b_filename = $b_segments[1];
+                        $b_handle = $b_segments[3];
+                        $b_seq = $b_segments[4];
+                        $b_handle_id = preg_replace('/^.*\//', '',$b_handle);
+                        $b_uri = './record/'.$b_handle_id.'/'.$b_seq.'/'.$b_filename;
+                        $thumbnailLink = "";
+
+                        if(isset($doc[$thumbnail_field])) {
+                            foreach ($doc[$thumbnail_field] as $thumbnail) {
+
+                                $t_segments = explode("##", $thumbnail);
+                                $t_filename = $t_segments[1];
+
+                                if ($t_filename === $b_filename . ".jpg") {
+
+                                    $t_handle = $t_segments[3];
+                                    $t_seq = $t_segments[4];
+                                    $t_uri = './record/'.$b_handle_id.'/'.$t_seq.'/'.$t_filename;
+
+                                    $thumbnailLink = '<a title = "' . $doc[$title_field][0] . '" class="fancybox" rel="group' . $j .'" href="' . $b_uri . '"> ';
+                                    $thumbnailLink .= '<img src = "'.$t_uri.'" class="search-thumbnail"" title="'. $doc[$title_field][0] .'" /></a>';
+                                }
+                            }
+                        }
+                        // there isn't a thumbnail so display the bitstream itself
+                        else {
+                            $thumbnailLink = '<a title = "' . $doc[$title_field][0] . '" class="fancybox" rel="group' . $j .'" href="' . $b_uri . '"> ';
+                            $thumbnailLink .= '<img src = "'.$b_uri.'" class="search-thumbnail" title="'. $doc[$title_field][0] .'" /></a>';
+                        }
+
+                        echo $thumbnailLink;
+                    }
+
+
+                    } //end if there are bitstreams ?>
+                </div>
+                <div class = "random-iteminfo">
+                    <h4><a href="./record/<?php echo $doc['id']?>"><?php echo $doc[$title_field][0]; ?></a></h4>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </li>
+
+    <?php } ?>
+
+    </ul>
+     </div>
+
+ </div>
+ <div class="clearfix"></div>
