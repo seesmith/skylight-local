1442bb6a6a52471dadd20aeceb290d76cf87b0ab
Claire Knowles <claire.knowles@ed.ac.uk>
2015-03-03 00:20:15 +0000

    LIB001-1052 New Iconics Site

--- theme/iconics/views/index.php
+++ theme/iconics/views/index.php
@@ -3,7 +3,7 @@
         <div class=tag-line>
         <h1>These iconic items are the most beautiful, important and unique treasures in the University’s collections.
         Some, like the Rashid al-Din manuscript, are known across the world.  Others, like Clement Litill’s charter
-        which founded the Library, are key to the story of our collections.  Some choices may surprise you!</h1>
+        which founded the Library, are key to the story of our collections.  Some choices may <a href="./">surprise</a> you!</h1>
         </div>
     </div>
 </div>
