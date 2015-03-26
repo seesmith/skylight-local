c4a9fdae77b4df0d2440a217edea01f3ced6815f
Claire Knowles <claire.knowles@ed.ac.uk>
2015-03-26 08:31:50 +0000

    LIB001-1212 Add participate info into CLD

--- static/clds/participate.php
+++ static/clds/participate.php
@@ -1,7 +1,10 @@
 <div class="content">
     <div class="content byEditor">
         <h1>How to participate</h1>
-      <p>link to ?
-        </p>
+      <p>There are a range of ways to get involved with our collections, and we have a number of volunteer opportunities
+          to suit all abilities and areas of interest.  If you would like to volunteer with us, please go to:
+          <a href="http://www.ed.ac.uk/schools-departments/information-services/library-museum-gallery/crc/volunteers-interns" target="_blank">volunteers and interns</a> for more information.
+
+      </p>
     </div>
 </div>
