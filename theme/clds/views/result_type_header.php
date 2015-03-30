bf294d4f75cbd8ca5d8736b48d190faefaeb2e51
ianthe <ianthe88@gmail.com>
2015-03-30 13:19:31 +0100

    LIB001-1219: Added videos to cld main headings

--- theme/clds/views/result_type_header.php
+++ theme/clds/views/result_type_header.php
@@ -1,43 +1,118 @@
 <?php
         //echo $search_url;
         if (!empty($search_url))
-        {
+{
 
-            if (strpos($search_url, 'Header:%22archives%22') > 0)
-            {
-                echo '<div class="results_header archives_header"><span class="results_text">Archive and Manuscript
-                collections contain documentary evidence for understanding a wide range of people, places, knowledge and
-                learning. They contain a strong focus on Scottish culture and detail pioneering research, and literary,
-                scientific and medical work through the University\'s own archive and Lothian Health Services Archive</span></div>';
-            }
-            elseif (strpos($search_url, 'Header:%22museums%22') > 0)
-            {
-                echo '<div class="results_header museums_header"><span class="results_text">Most collections support scholarly
-                research, some tell the story of the University’s past, some are used daily as teaching collections, while
-                others, such as the Natural History Collections, are DNA libraries, while yet others glorify corporate spaces
-                - they are all are part of the University’s rich cultural holdings.</span></div>';
-            }
-            elseif (strpos($search_url, 'Header:%22rarebooks%22') > 0)
-            {
-                echo '<div class="results_header rare_header"><span class="results_text">We have about 400,000 rare books and manuscripts, many found nowhere else.  Our earliest handwritten
-                book is the 11th century Celtic Psalter; the earliest printed book is a woodblock Chinese commentary produced
-                in 1440.  The collection includes the libraries of Enlightenment economist Adam Smith and modern Scottish
-                writer Hugh MacDiarmid.</span></div>';
-            }
-            elseif (strpos($search_url, 'Header:%22art%22') > 0)
-            {
-                echo '<div class="results_header art_header"><span class="results_text">The Art Collection contains over 5,000 items which reflect the history of the University, the city
-                and Scotland and also supports world-leading research and teaching at Edinburgh. The collection comprises
-                an astonishing range of objects, spanning two millennia and a multitude of artistic periods.</span></div>';
-            }
-            elseif (strpos($search_url, 'Header:%22mimed%22') > 0)
-            {
-                echo '<div class="results_header mimed_header"><span class="results_text">The Musical Instrument Collection
-                contains over 5,500 items, covering the history of musical instruments from c1550 to the present day.  Instruments of all
-                types and traditions can be found, including some of the world’s most iconic, and much revered, examples of their type. </span></div>';
-            }
-        }
+if (strpos($search_url, 'Header:%22archives%22') > 0)
+{ ?>
+    <div class="results_header archives_header">
+        <div class="results_text">Archive and Manuscript collections contain documentary evidence for understanding
+            a wide range of people, places, knowledge and learning. They contain a strong focus on Scottish culture
+            and detail pioneering research, and literary,scientific and medical work through the University's own
+            archive and Lothian Health Services Archive
+        </div>
 
+        <div class="flowplayer" data-analytics="<?php echo $ga_code ?>"
+             title="Introduction to Archives by Archives Manager, Rachel Hosker">
+            <video id="video-archives" title="Introduction to Archives by Archives Manager, Rachel Hosker" controls preload="true">
+                <?php if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') == false) { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-006.mp4" type="video/mp4"/>
+                <?php } else { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-006.webm" type="video/webm"/>
+                <?php } ?>
+                Video loading...'
+            </video>
+        </div>
+    </div>
+
+<?php } elseif (strpos($search_url, 'Header:%22museums%22') > 0) { ?>
+
+    <div class="results_header museums_header">
+        <div class="results_text">Most collections support scholarly research, some tell the story of the
+            University's past, some are used daily as teaching collections, while others, such as the Natural
+            History Collections, are DNA libraries, while yet others glorify corporate spaces - they are all are
+            part of the University’s rich cultural holdings.
+        </div>
+
+        <div class="flowplayer" data-analytics="<?php echo $ga_code ?>"
+             title="Introduction to Museums by Head of Museums, Jacky MacBeath">
+            <video id="video-museums" title="Introduction to Museums by Head of Museums, Jacky MacBeath" controls preload="true">
+                <?php if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') == false) { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-004.mp4" type="video/mp4"/>
+                <?php } else { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-004.webm" type="video/webm"/>
+                <?php } ?>
+                Video loading...'
+            </video>
+        </div>
+    </div>
+
+<?php } elseif (strpos($search_url, 'Header:%22rarebooks%22') > 0) { ?>
+
+    <div class="results_header rare_header">
+        <div class="results_text">We have about 400,000 rare books and manuscripts, many found nowhere else. Our
+            earliest handwritten book is the 11th century Celtic Psalter; the earliest printed book is a woodblock
+            Chinese commentary produced in 1440. The collection includes the libraries of Enlightenment economist
+            Adam Smith and modern Scottish writer Hugh MacDiarmid.
+        </div>
+
+        <div class="flowplayer" data-analytics="<?php echo $ga_code ?>"
+             title="Introduction to Rare Books by Head of Special Collections, Joseph Marshall">
+            <video id="video-rarebooks" title="Introduction to Rare Books by Head of Special Collections, Joseph Marshall" controls preload="true">
+                <?php if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') == false) { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-005.mp4" type="video/mp4"/>
+                <?php } else { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-005.webm" type="video/webm"/>
+                <?php } ?>
+                Video loading...'
+            </video>
+        </div>
+    </div>
+
+<?php } elseif (strpos($search_url, 'Header:%22art%22') > 0) { ?>
+
+    <div class="results_header art_header">
+        <div class="results_text">The Art Collection contains over 5,000 items which reflect the history of the
+            University, the city and Scotland and also supports world-leading research and teaching at Edinburgh. The
+            collection comprises an astonishing range of objects, spanning two millennia and a multitude of artistic
+            periods.
+        </div>
+
+        <div class="flowplayer" data-analytics="<?php echo $ga_code ?>"
+             title="Introduction to the Art Collections by Curator Neil Lebeter">
+            <video id="video-art" title="Introduction to the Art Collections by Curator Neil Lebeter" controls preload="true">
+                <?php if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') == false) { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-001.mp4" type="video/mp4"/>
+                <?php } else { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-001.webm" type="video/webm"/>
+                <?php } ?>
+                Video loading...'
+            </video>
+        </div>
+    </div>
+
+<?php } elseif (strpos($search_url, 'Header:%22mimed%22') > 0) { ?>
+
+    <div class="results_header mimed_header">
+        <div class="results_text">The Musical Instrument Collection contains over 5,500 items, covering the history of
+            musical instruments from c1550 to the present day. Instruments of all types and traditions can be found,
+            including some of the world’s most iconic, and much revered, examples of their type.
+        </div>
+
+        <div class="flowplayer" data-analytics="<?php echo $ga_code ?>"
+             title="Introduction to MIMEd by MIMEd Pricipal Curator, Darryl Martin">
+            <video id="video-mimed" title="Introduction to MIMEd by MIMEd Pricipal Curator, Darryl Martin" controls preload="true">
+                <?php if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') == false) { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-002.mp4" type="video/mp4"/>
+                <?php } else { ?>
+                    <source src="<?php echo base_url(); ?>videos/0051011v-002.webm" type="video/webm"/>
+                <?php } ?>
+                Video loading...'
+            </video>
+        </div>
+    </div>
+<?php }
+}
 ?>
 
 
