1742daf72270c1b8f2972ea118f962e0ea46055c
Ianthe Sutherland <ianthe88@gmail.com>
2015-04-29 14:45:27 +0100

    LIB001-1281: Improving link to metadata games - specific image

--- theme/iconics/views/record.php
+++ theme/iconics/views/record.php
@@ -18,6 +18,7 @@ $type = 'Unknown';
 $mainImageTest = false;
 $numThumbnails = 0;
 $bitstreamLinks = array();
+$image_id = "";
 
 if(isset($solr[$type_field])) {
     $type = "media-" . strtolower(str_replace(' ','-',$solr[$type_field][0]));
@@ -45,6 +46,7 @@ if(isset($solr[$bitstream_field]) && $link_bitstream) {
 
         $b_segments = explode("##", $bitstream);
         $b_filename = $b_segments[1];
+        $image_id = substr($b_filename,0,7);
         $b_handle = $b_segments[3];
         $b_seq = $b_segments[4];
         $b_handle_id = preg_replace('/^.*\//', '',$b_handle);
@@ -285,7 +287,12 @@ if(isset($solr[$bitstream_field]) && $link_bitstream) {
                 $lower_orig_filter = urlencode($lower_orig_filter);
                 echo '<span class="crowd-tag">' . '<a href="./search/*:*/Tags:%22'.$lower_orig_filter.'%7C%7C%7C'.$orig_filter.'%22"><i class="fa fa-tags fa-lg">&nbsp;</i>'.$tag.'</a>' . '</span>';
             } ?>
-            <div class="crowd-info">Add more tags at <a href="http://librarylabs.ed.ac.uk/games/" target="_blank" title="University of Edinburgh, Library Labs Metadata Games Home">Library Labs Games</a> </div>
+            <div class="crowd-info">
+                <form id="libraylabs" method="post" action="http://librarylabs.ed.ac.uk/games/gameCrowdSourcing.php" target="_blank">
+                    <input type="hidden" name="image_id" value="<?php echo $image_id ?>">
+                    Add more tags at <a href="#" onclick="document.forms[1].submit();return false;" title="University of Edinburgh, Library Labs Metadata Games">Library Labs Games</a>
+                </form>
+            </div>
         </div>
 
     <?php } ?>
