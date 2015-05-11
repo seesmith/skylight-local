09d6ff72460e0f4568d1c3526c14ad7a065f6af9
Claire Knowles <claire.knowles@ed.ac.uk>
2015-05-11 23:49:27 +0100

    LIB001-1288 Create carousel slide for iconics

--- theme/clds/views/index.php
+++ theme/clds/views/index.php
@@ -13,6 +13,14 @@
         <!-- Wrapper for slides -->
         <div class="carousel-inner">
             <div class="item active">
+                <a href="<?php echo base_url(); ?>iconics">
+                    <img src="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/iconicsslide.png" title="University of Edinburgh Iconics Collection" alt="University of Edinburgh Iconics">
+                    <div class="carousel-caption caption-iconic" onmouseover="this.style.background='#005784';this.style.color='#FFF'" onmouseout="this.style.background='#FFFFFF';this.style.color='#005784'">
+                        University of Edinburgh Iconics
+                    </div>
+                </a>
+            </div>
+            <div class="item">
                 <a href="<?php echo base_url(); ?>art">
                 <img src="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/artslide.png" title="University of Edinburgh Art Collection" alt="University of Edinburgh Art Collection">
                 <div class="carousel-caption caption-art" onmouseover="this.style.background='#005784';this.style.color='#FFF'" onmouseout="this.style.background='#FFFFFF';this.style.color='#005784'">
@@ -28,12 +36,13 @@
                 </div>
                 </a>
             </div>
+
             <div class="item">
-                <a href="<?php echo base_url(); ?>mimed/search/*/Collection:%22raymond+russell+collection%7C%7C%7CRaymond+Russell+Collection%22">
-                <img src="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/harpsichordslide.png" title="Raymond Russell Keyboards Collection" alt="Raymond Russell Keyboards Collection">
-                <div class="carousel-caption caption-russell" onmouseover="this.style.background='#005784';this.style.color='#FFF'" onmouseout="this.style.background='#FFFFFF';this.style.color='#005784'">
-                    Raymond Russell Keyboards Collection
-                </div>
+                <a href="https://exhibitions.ed.ac.uk">
+                    <img src="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/exhibitionsslide.png" title="University of Edinburgh Exhibitions" alt="University of Edinburgh Exhibitions">
+                    <div class="carousel-caption caption-exhibitions" onmouseover="this.style.background='#005784';this.style.color='#FFF'" onmouseout="this.style.background='#FFFFFF';this.style.color='#005784'">
+                        University of Edinburgh Exhibitions
+                    </div>
                 </a>
             </div>
             <div class="item">
@@ -41,14 +50,6 @@
                     <img src="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/calendarslide.png" title="University of Edinburgh Calendars" alt="University of Edinburgh Calendars">
                     <div class="carousel-caption caption-calendars" onmouseover="this.style.background='#005784';this.style.color='#FFF'" onmouseout="this.style.background='#FFFFFF';this.style.color='#005784'">
                         University of Edinburgh Calendars
-                    </div>
-                </a>
-            </div>
-            <div class="item">
-                <a href="https://exhibitions.ed.ac.uk">
-                    <img src="<?php echo base_url(); ?>theme/<?php echo $this->config->item('skylight_theme'); ?>/images/exhibitionsslide.png" title="University of Edinburgh Exhibitions" alt="University of Edinburgh Exhibitions">
-                    <div class="carousel-caption caption-exhibitions" onmouseover="this.style.background='#005784';this.style.color='#FFF'" onmouseout="this.style.background='#FFFFFF';this.style.color='#005784'">
-                        University of Edinburgh Exhibitions
                     </div>
                 </a>
             </div>
