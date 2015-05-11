d911d6cfb4a49efebbfb9b572dff1bfca8515ade
Claire Knowles <claire.knowles@ed.ac.uk>
2015-05-12 00:28:20 +0100

    LIB001-1312 Add library labs link to all record pages

--- theme/art/views/record.php
+++ theme/art/views/record.php
@@ -293,7 +293,7 @@ if(isset($solr[$bitstream_field]) && $link_bitstream) {
         <div class="crowd-info">
             <form id="libraylabs" method="get" action="http://librarylabs.ed.ac.uk/games/gameCrowdSourcing.php" target="_blank">
                 <input type="hidden" name="image_id" value="<?php echo $image_id ?>">
-                <input type="hidden" name="theme" value="art">
+                <input type="hidden" name="theme" value="classic">
                 Add tags to this image at <a href="#" onclick="document.forms[1].submit();return false;" title="University of Edinburgh, Library Labs Metadata Games">Library Labs Games</a> (Create a login at <a href="https://www.ease.ed.ac.uk/friend/">Edinburgh Friend Account</a>)
             </form>
         </div>
