1dfacc739dc0c6ca997fc9fe28d61c232974855a
Claire Knowles <claire.knowles@ed.ac.uk>
2015-09-15 16:58:46 +0100

    LIB001-1462 Piccolo online responsive design - replace bootstrap location

--- theme/rocks/views/header.php
+++ theme/rocks/views/header.php
@@ -46,7 +46,7 @@
         <script src="<?php echo base_url()?>assets/modernizr/modernizr-1.7.min.js"></script>
         <script src="<?php echo base_url()?>assets/jquery-1.11.0/jquery-1.11.0.min.js"></script>
         <script src="<?php echo base_url()?>assets/jquery-ui-1.10.4/ui/minified/jquery-ui.min.js"></script>
-        <script src="<?php echo base_url()?>assets/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
         <script src="<?php echo base_url()?>assets/jquery-1.11.0/jcarousel/jquery.jcarousel.min.js"></script>
         <script src="http://www.google-analytics.com/analytics.js"></script>
 
