290c580450941ea52f091ef406b10177332112b5
Scott Renton <scott.renton@ed.ac.uk>
2015-11-13 09:44:34 +0000

    LIB001-1536 St Cecilia App changes

---
+++ static/stcecilia/about.php
@@ -0,0 +1,70 @@
+<div class="content">
+    <div class="content byEditor">
+        <h1>About the University of Edinburgh Art Collection</h1>
+
+        <p>The University holds around 2,500 works of art. The Art Collection is now an amalgamation of the University of Edinburgh’s original art collection, which spans some 400 years of collecting, and the Edinburgh College of Art collection of prints, drawings, paintings and sculpture. The ECA collection tells a unique story of the artist output of the College, particularly in the early to mid-20th Century. Some of the most respected names in Scottish art appear, such as <a href="./search/Peploe">Samuel Peploe</a>, <a href="./search/Bellany">John Bellany</a>, <a href="./search/Redpath">Anne Redpath</a> and <a href="./search/Blackadder">Elizabeth Blackadder</a>.</p>
+        <p>
+            The Art Collection has as its centrepiece the Torrie Collection of 17th century Dutch and Italian Masters
+            with works by <a href="./search/Ruisdael">Ruisdael</a>, <a href="./search/ten%20Oever">ten Oever</a>,
+            <a href="./search/Van%20der%20Meulen">Van der Meulen</a>, <a href="./search/Pynacker">Pynacker</a>,
+            <a href="./search/Rosa">Rosa</a> and <a href="./search/Rvan%20de%20Velde">Rvan de Velde</a>. The University also holds
+            the second largest collection of portraits in Scotland ranging from 17th century portraits of
+            <a href="./search/Napier">John Napier</a> and <a href="./search/John%20Knox">John Knox</a>
+            to the recent dynamic painting of <a href="./search/Peter%20Higgs">Peter Higgs</a>
+            by Ken Currie. Other portrait artists represented are
+            <a href="./search/Henry%20Raeburn">Sir Henry Raeburn</a>, <a href="./search/Stanley%20Cursiter">Stanley Cursiter</a>,
+            <a href="./search/George%20Reid">Sir George Reid</a>, <a href="./search/James%20Cowie">James Cowie</a> and
+            <a href="./search/Victoria%20Crowe">Victoria Crowe</a>.
+        </p>
+        <p>
+            A large percentage of the Art Collection is on display enhancing the public, staff and student spaces of the
+            University. A number of works from the Torrie Collection are on long-term loan to the National Galleries of Scotland.
+            The principal areas of display include Edinburgh College of Art, Old College, Playfair Library, Raeburn Room,
+            the University of Edinburgh Library, New College, McEwan Hall and the Royal (Dick) School of Veterinary Studies.
+        </p>
+        <p>
+            As well as supporting research at all levels of the University, we believe that the uniqueness of our art collections
+            lies in their innovative use. We seek to interpret and display collections in new and exciting ways, introducing
+            contemporary insights and conversations to constantly reinvigorate debate. This involves working closely with
+            Edinburgh College of Art and The Talbot Rice Gallery to ensure that collections are developed through contemporary
+            collecting and also utilised by some of the world’s leading contemporary artists, academics and curators and
+            presented to as wide an audience as possible.
+        </p>
+
+
+        <h1>Art Collections Online</h1>
+        <p>
+            We aim to represent all of the University Art Collection online through this resource. This is an ongoing process as the existing collection continues to be catalogued and photographed and also as new acquisitions are brought in. Images accompany records where there is an existing image file of sufficient quality and images will be added to existing records as part of wider cataloguing efforts. If there is a particular work that is of interest that does not have an accompanying image, please contact us directly on the details below.
+        </p>
+        <h1>University Collections</h1>
+        <p>
+            Special Collections include manuscripts and archives, rare books, photographs, printed and handwritten music, architectural drawings, theses and microfilms. The collections relate to University of Edinburgh and its history, and to Scotland and the wider world.</p>
+        <p>In total we have about 35 kilometres of historic material, much of which cannot be found anywhere else in the world. <a href="http://collections.ed.ac.uk" Title="University Collections Home">collections.ed.ac.uk</a>
+        </p>
+        <h1>Centre for Research Collections</h1>
+        <p>
+            Further information about the <a href="http://www.ed.ac.uk/schools-departments/information-services/library-museum-gallery/crc" title="Centre for Research Collections" target="_blank">Centre for Research Collections</a>.
+        </p>
+        <h1>BBC Your Paintings</h1>
+        <p>
+        <a href="http://www.bbc.co.uk/arts/yourpaintings/galleries/locations/university-of-edinburgh-5095" title="" >University Collections on BBC Your Paintings</a><br/>
+        <a href="http://www.bbc.co.uk/arts/yourpaintings/galleries/locations/eca-part-of-university-of-edinburgh-fine-art-collection-7553" title="" >Edinburgh College of Art Collections on BBC Your Paintings</a>
+        <p/>
+        <h1>Contact Details</h1>
+        <p>
+            For further information about the University of Edinburgh Art Collections please contact:
+        </p>
+        <p><strong>Neil Lebeter</strong><br />
+            Art Collections Curator<br />
+            University of Edinburgh,<br />
+            Main Library,<br />
+            30 George Square,<br />
+            Edinburgh,<br />
+            EH8 9LJ<br />
+            <a class="email" href="mailto:neil.lebeter@ed.ac.uk">neil.lebeter@ed.ac.uk</a><br />
+            <br />
+            <a href="https://twitter.com/UoEArtArchives" title="Art Collections on Twitter">Art Collections on Twitter</a><br />
+            <a href="http://uoeartandarchives.tumblr.com/" title="Art Collections on Tumblr">Art Collections on Tumblr</a><br />
+
+    </div>
+</div>
