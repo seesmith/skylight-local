f118afa99679956dc4cfc4efac6dc280c2c36c48
Claire Knowles <claire.knowles@ed.ac.uk>
2015-11-20 00:12:51 +0000

    LIB001-1547 new Marjorie Kennedy Fraser collection

---
+++ static/mkf/about.php
@@ -0,0 +1,34 @@
+<div class="content">
+    <div class="container">
+        <h2>About the University Collections</h2>
+        <p>
+            Special Collections include manuscripts and archives, rare books, photographs, printed and handwritten music,
+            architectural drawings, theses and microfilms. The collections relate to University of Edinburgh and its history,
+            and to Scotland and the wider world.
+        </p>
+        <p>
+            In total we have about 35 kilometres of historic material, much of which cannot be found anywhere else in the world.
+        </p>
+        <h2>University Collections Online</h2>
+        <p>
+            We aim to represent all of the University's rare and unique collections on this site, where possible there are links to websites where you can view the items within the collection.
+        </p>
+        <h2>Centre for Research Collections</h2>
+        <p>
+            Further information about the <a href="http://www.ed.ac.uk/schools-departments/information-services/library-museum-gallery/crc" title="Centre for Research Collections" target="_blank">Centre for Research Collections</a>.
+        </p>
+        <h2>Contact</h2>
+        <p>
+            Centre for Research Collections<br />
+            Edinburgh University Library,<br />
+            George Square,<br />
+            Edinburgh,<br />
+            EH8 9LJ<br />
+            Tel:: +44(0)131 650 8379<br />
+            Fax: +44(0)131 650 2922<br />
+            <a class="email" href="mailto:is-crc@ed.ac.uk">is-crc@ed.ac.uk</a><br />
+
+        </p>
+        <br />
+    </div>
+</div>
