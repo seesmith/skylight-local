c0286513bf23fee8f91ffc10b797fe1099dfddc3
Claire Knowles <claire.knowles@ed.ac.uk>
2015-12-02 08:17:57 +0000

    LIB001-1584 Update Flowplayer to version 6.0.4 change audio and video tags to just include controls

--- theme/art/views/record.php
+++ theme/art/views/record.php
@@ -102,9 +102,7 @@ if(isset($solr[$bitstream_field]) && $link_bitstream) {
         }
         else if ((strpos($b_uri, ".mp3") > 0) or (strpos($b_uri, ".MP3") > 0)) {
 
-            $audioLink .= '<audio id="audio-' . $b_seq;
-            $audioLink .= '" title="' . $record_title . ": " . $b_filename . '" ';
-            $audioLink .= 'controls preload="true" width="600">';
+            $audioLink .= '<audio controls>';
             $audioLink .= '<source src="' . $b_uri . '" type="audio/mpeg" />Audio loading...';
             $audioLink .= '</audio>';
             $audioFile = true;
@@ -118,8 +116,7 @@ if(isset($solr[$bitstream_field]) && $link_bitstream) {
             if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') == false) {
 
                 $videoLink .= '<div class="flowplayer" data-analytics="' . $ga_code . '" title="' . $record_title . ": " . $b_filename . '">';
-                $videoLink .= '<video id="video-' . $b_seq. '" title="' . $record_title . ": " . $b_filename . '" ';
-                $videoLink .= 'controls preload="true" width="600">';
+                $videoLink .= '<video controls>';
                 $videoLink .= '<source src="' . $b_uri . '" type="video/mp4" />Video loading...';
                 $videoLink .= '</video>';
                 $videoLink .= '</div>';
@@ -135,8 +132,7 @@ if(isset($solr[$bitstream_field]) && $link_bitstream) {
             if (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') == true) {
 
                 $videoLink .= '<div class="flowplayer" data-analytics="' . $ga_code . '" title="' . $record_title . ": " . $b_filename . '">';
-                $videoLink .= '<video id="video-' . $b_seq. '" title="' . $record_title . ": " . $b_filename . '" ';
-                $videoLink .= 'controls preload="none" width="600">';
+                $videoLink .= '<video controls>';
                 $videoLink .= '<source src="' . $b_uri . '" type="video/webm" />Video loading...';
                 $videoLink .= '</video>';
                 $videoLink .= '</div>';
