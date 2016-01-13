4fb118d48370a094c1042b3575be26f91275a347
Claire Knowles <claire.knowles@ed.ac.uk>
2016-01-13 11:47:52 +0000

    LIB001-1590 Audio collection text

--- theme/audio/views/index.php
+++ theme/audio/views/index.php
@@ -2,11 +2,27 @@
     <!-- todo replace two with sass or less -->
 <div class="container">
     <div class="jumbotron hidden-xs hidden-sm">
-            <p>Audio Recordings</p>
+        <p>We have a number of archival collections that contain audio in various formats.  Making these available for
+            research presents a number of challenges and we are currently developing a strategy to digitise and make available online.
+        </p><p>
+            This resource currently contains copies of recordings made singer, composer, arranger and collector,
+            Marjory Kennedy-Fraser (1857-1930).  These recordings were made in the islands of the Scottish Hebrides
+            in the early 20th century.  They are currently only available in their 'raw' digitised form, until time
+            and resource allows us to create enhanced versions to accompany them.
+        </p><p>
+            We plan to add other digitised audio to this resource in due course.</p>
     </div>
     <div class="jumbotron hidden-md hidden-lg">
         <div class="jumbotron_sml">
-        <p>Audio Recordings </p>
+        <p>We have a number of archival collections that contain audio in various formats.  Making these available for
+            research presents a number of challenges and we are currently developing a strategy to digitise and make available online.
+        </p><p>
+            This resource currently contains copies of recordings made singer, composer, arranger and collector,
+                Marjory Kennedy-Fraser (1857-1930).  These recordings were made in the islands of the Scottish Hebrides
+                in the early 20th century.  They are currently only available in their 'raw' digitised form, until time
+                and resource allows us to create enhanced versions to accompany them.
+        </p><p>
+            We plan to add other digitised audio to this resource in due course.</p>
         </div>
     </div>
     <div class="items-link">
