437c95415aab2ef0bd0c8cd14efe39451820f26b
Scott Renton <scott.renton@ed.ac.uk>
2017-01-25 12:09:12 +0000

    LIB001-1840 Iconics changes plus St Cecilia to master

--- theme/iconics/views/index.php
+++ theme/iconics/views/index.php
@@ -1,10 +1,15 @@
 <div class="record">
     <div class="content byEditor">
         <div class="tag-line">
-        <h1>These iconic items are the most beautiful, important and unique treasures in the University’s collections.
-        Some, like the Rashid al-Din manuscript, are known across the world.  Others, like Clement Litill’s charter
-        which founded the Library, are key to the story of our collections.  Some choices may <a href="./">surprise</a> you!
-        &nbsp; &nbsp;<a href="<?php echo base_url(); ?>iconics/search">View All</a></h1>
+            <h1>These iconic items are the most beautiful, important and unique treasures in the University’s collections.
+            Some, like the Rashid al-Din manuscript, are known across the world.  Others, like Clement Litill’s charter
+            which founded the Library, are key to the story of our collections.  Some choices may <a href="./">surprise</a> you!
+            &nbsp; &nbsp;<a href="<?php echo base_url(); ?>iconics/search">View All</a>
+                This collection is IIIF-compliant.
+                <a target="_blank" href="http://images.is.ed.ac.uk/luna/servlet/iiif/collection/g/376">
+                    <img src="http://manifests.britishart.yale.edu/logo-iiif.png" alt="IIIF Manifest">
+                </a>&nbsp;Right-click, Copy Link on the logo to get the full IIIF manifest for the collection.
+            </h1>
         </div>
     </div>
 </div>
