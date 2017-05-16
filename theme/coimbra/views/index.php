516881ff1ae64a1a1f9756948618ec37467cfb4e
Claire Knowles <claire.knowles@ed.ac.uk>
2017-05-16 19:33:03 +0100

    LIB001-1893 Coimbra collection updates - Updated homepage images to match selection of items

--- theme/coimbra/views/index.php
+++ theme/coimbra/views/index.php
@@ -4,11 +4,11 @@
     <div class="parallax">
     </div>
     <ul class="main-categories">
-        <li class="col-xs-6"><a href='./search/"The+Arts"'><div class="border-top"></div>The Arts</a></li>
-        <li class="col-xs-6"><a href='./search/"Natural+Sciences"'><div class="border-top"></div>Natural Sciences</a></li>
         <li class="col-xs-6"><a href='./search/"Voyage"'><div class="border-top"></div>Voyage</a></li>
         <li class="col-xs-6"><a href='./search/"Discovery"'><div class="border-top"></div>Discovery</a></li>
         <li class="col-xs-6"><a href='./search/"Faith"'><div class="border-top"></div>Faith</a></li>
+        <li class="col-xs-6"><a href='./search/"The+Arts"'><div class="border-top"></div>The Arts</a></li>
+        <li class="col-xs-6"><a href='./search/"Natural+Sciences"'><div class="border-top"></div>Natural Sciences</a></li>
         <li class="col-xs-6"><a href='./search/"War+Peace"'><div class="border-top"></div>War & Peace</a></li>
         <li class="col-xs-12"><a href='./search/"The+Wunderkammer"'><div class="border-top"></div>The Wunderkammer</a></li>
     </ul>
