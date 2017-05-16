61b637a7fbf31558c749003f1dec9b0fd7ca3559
Claire Knowles <seesmith@users.noreply.github.com>
2017-05-16 23:29:44 +0100

    Coimbra fix (#454)
    * LIB001-1858 Dott Feedback
    
    * LIB001-1858 Dott Feedback rename ArchivesSpace links to Archives Online
    
    * LIB001-1858 Dott Feedback rename site from dott to lhsacasenotes
    
    * LIB001-1893 Coimbra collection updates
    
    * LIB001-1893 Coimbra collection updates
    
    * LIB001-1858 Dott Feedback rename site from dott to lhsacasenotes
    
    * LIB001-1893 Coimbra collection updates - Favicon
    
    * LIB001-1893 Coimbra collection updates - Uni Logo in footer as link
    
    * LIB001-1893 Coimbra collection updates - Split metadata
    
    * LIB001-1893 Coimbra collection updates - title of page and link in new tab
    
    * LIB001-1893 Coimbra collection updates - Updated homepage images to match selection of items
    
    * LIB001-1893 Coimbra collection updates - Make uri links
    
    * LIB001-1893 Coimbra collection updates - Updated homepage images to match selection of items
    
    * LIB001-1893 Coimbra collection updates - record { fix
    
    * LIB001-1893 Coimbra collection updates - Updated homepage order

--- theme/coimbra/views/index.php
+++ theme/coimbra/views/index.php
@@ -4,11 +4,11 @@
     <div class="parallax">
     </div>
     <ul class="main-categories">
-        <li class="col-xs-6"><a href='./search/*:*/Category:%22the+arts%7C%7C%7CThe+Arts%22'><div class="border-top"></div>The Arts</a></li>
-        <li class="col-xs-6"><a href='./search/*:*/Category:%22natural+sciences%7C%7C%7CNatural+Sciences%22'><div class="border-top"></div>Natural Sciences</a></li>
-        <li class="col-xs-6"><a href='./search/*:*/Category:%22vouyage%7C%7C%7CVoyage%22'><div class="border-top"></div>Voyage</a></li>
+        <li class="col-xs-6"><a href='./search/*:*/Category:%22voyage%7C%7C%7CVoyage%22'><div class="border-top"></div>Voyage</a></li>
         <li class="col-xs-6"><a href='./search/*:*/Category:%22discovery%7C%7C%7CDiscovery%22'><div class="border-top"></div>Discovery</a></li>
         <li class="col-xs-6"><a href='./search/*:*/Category:%22faith%7C%7C%7CFaith%22'><div class="border-top"></div>Faith</a></li>
+        <li class="col-xs-6"><a href='./search/*:*/Category:%22the+arts%7C%7C%7CThe+Arts%22'><div class="border-top"></div>The Arts</a></li>
+        <li class="col-xs-6"><a href='./search/*:*/Category:%22natural+sciences%7C%7C%7CNatural+Sciences%22'><div class="border-top"></div>Natural Sciences</a></li>
         <li class="col-xs-6"><a href='./search/*:*/Category:%22war+peace%7C%7C%7CWar+Peace%22'><div class="border-top"></div>War & Peace</a></li>
         <li class="col-xs-12"><a href='./search/*:*/Category:%22the+wunderkammer%7C%7C%7CThe+Wunderkammer%22'><div class="border-top"></div>The Wunderkammer</a></li>
     </ul>
--- theme/coimbra/views/record.php
+++ theme/coimbra/views/record.php
@@ -76,13 +76,13 @@ $jsonwidth = $jobj['width'];
                             echo '<a href="./search/*:*/' . $key . ':%22'.$lower_orig_filter.'%7C%7C%7C'.$orig_filter.'%22">'.$metadatavalue.'</a>';
                         }
                         else {
-                        }
-                             if (stripos($element, "uri") !== FALSE) {
+                            if (stripos($element, "uri") !== FALSE) {
                                 echo '<a href="' . $solr[$element][0] . '" title="URL Links for item" target="_blank">' . $solr[$element][0] . '</a>';
 
                             }
-                        else {
-                            echo $solr[$element][0];
+                            else {
+                                echo $solr[$element][0];
+                            }
                         }
 
                     }
