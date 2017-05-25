0693f85fcf51d142c66bf7314a383b8cf5796387
Claire Knowles <seesmith@users.noreply.github.com>
2017-05-25 10:21:55 +0100

    LIB001-1907 Create coming soon on exhibitions site (#465)
    * LIB001-1907 Create coming soon on exhibitions site
    
    * LIB001-1893 Coimbra collection updates - Back button hover
    
    * LIB001-1893 Coimbra collection updates - pinpoint and footer

---
+++ static/exhibitions/comingsoon.php
@@ -0,0 +1,33 @@
+<div class="content">
+    <div class="content byEditor">
+
+        <h1>Future Exhibitions:</h1>
+            <div>
+                <h2><a href="./highlandstohindustan" alt="Highlands to Hindustan" title="Highlands to Hindustan Information Page">Highlands to Hindustan: Indian Art from University of Edinburgh Collections</a></h2>
+                <p>28th July 2017 - 28th October 2017</p>
+                <a href="./highlandstohindustan" alt="Highlands to Hindustan" title="Highlands to Hindustan">
+                <div id="highlands-banner"></div>
+                </a>
+
+                <p>
+                    The University of Edinburgh enjoys a long-standing
+                    and productive relationship with India and an
+                    exhibition in the Library opening during the Fringe Festival
+                    this year, the 70th anniversary of Indian Independence,
+                    provides an excellent opportunity to showcase and
+                    celebrate the University’s historic collections relating
+                    to the sub-continent.
+                </p>
+                <p>
+                    These objects – books, manuscripts, sculptures and
+                    paintings – collected over a period of 300 years, span
+                    two millennia of art and culture and are rich both in
+                    variety and depth. Most of the objects have never been
+                    exhibited in public before; this will be the first ever
+                    public exhibition of University Collections devoted
+                    to the art and culture of South Asia.
+                </p>
+            <p>Curated By: Dr. Yuthika Sharma, Lecturer in Indian/South Asian Art, Edinburgh College of Art</p>
+        </div>
+    </div>
+</div>
