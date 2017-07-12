1bd21acccd14cd5b35964f805a68d698d68ba29a
Claire Knowles <claire.knowles@ed.ac.uk>
2017-07-12 09:50:40 +0100

    LIB001-1948 About page not working in St Cecilia's Hall

---
+++ static/stcecilias/about.php
@@ -0,0 +1,25 @@
+<div class="content">
+    <div class="content byEditor">
+        <h1>About the museum</h1>
+
+        <h2>History</h2>
+
+        <p>Originally opened in 1763 , St Cecilia’s Hall is the oldest purpose-built concert hall in Scotland. It is the
+            only place in the world where it is possible to hear eighteenth century music in an eighteenth century concert
+            hall played on eighteenth century instruments. Owned by The University of Edinburgh, it contains one of the
+            world’s most important collections of historic musical instruments, many of which remain playable in a concert setting.</p>
+
+        <p>The new St Cecilia’s Hall museum brings this collection under one roof, allowing the story of the collection
+            as a whole to be presented and enabling visitors to truly engage with the objects and their stories. The
+            elegant oval concert hall on the first floor of the building has been in continuous use since it opened in
+            the eighteenth century and remains at the heart of the Georgian building, but is now complemented by four
+            galleries on both ground and first floor. The restoration work, which began work in 2015, allows the University
+            to offer more performances, enhance access by increasing the opening hours of the museum and provide new
+            ways for more visitors to engage, participate and learn about its world-class collection of instruments.</p>
+
+        <p>The museum re-opened its doors to the public in May 2017. Find out more about visiting at the museum site
+            <a href="http://www.stcecilias.ed.ac.uk" title="Visit St Cecilia's Hall" target="_blank">St Cecilia's Hall</a></p>
+
+    </div>
+</div>
+</div>
