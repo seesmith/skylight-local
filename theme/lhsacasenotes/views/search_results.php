befc3da429eb2d548281f6f191a7b24974d9f5fe
Claire Knowles <claire.knowles@ed.ac.uk>
2017-07-18 12:18:13 +0100

    LIB001-1858 Dott Feedback -search results page

--- theme/lhsacasenotes/views/search_results.php
+++ theme/lhsacasenotes/views/search_results.php
@@ -1,156 +1,133 @@
 
-    <?php
+<?php
 
-        // Set up some variables to easily refer to particular fields you've configured
-        // in $config['skylight_searchresult_display']
+// Set up some variables to easily refer to particular fields you've configured
+// in $config['skylight_searchresult_display']
 
-        $title_field = $this->skylight_utilities->getField('Title');
-        $author_field = $this->skylight_utilities->getField('Creator');
-        $date_field = $this->skylight_utilities->getField('Date');
-        $type_field = $this->skylight_utilities->getField('Type');
-        $abstract_field = $this->skylight_utilities->getField('Agents');
-        $subject_field = $this->skylight_utilities->getField('Subject');
+$title_field = $this->skylight_utilities->getField('Title');
+$author_field = $this->skylight_utilities->getField('Creator');
+$date_field = $this->skylight_utilities->getField('Date');
+$type_field = $this->skylight_utilities->getField('Type');
+$abstract_field = $this->skylight_utilities->getField('Agents');
+$subject_field = $this->skylight_utilities->getField('Subject');
 
-        $base_parameters = preg_replace("/[?&]sort_by=[_a-zA-Z+%20. ]+/","",$base_parameters);
-        if($base_parameters == "") {
-            $sort = '?sort_by=';
-        }
-        else {
-            $sort = '&sort_by=';
-        }
-    ?>
-    <div class="listing-filter">
-        <span class="no-results">
-        <strong><?php echo $startrow ?>-<?php echo $endrow ?></strong> of
-            <strong><?php echo $rows ?></strong> results
-        </span>
+$base_parameters = preg_replace("/[?&]sort_by=[_a-zA-Z+%20. ]+/","",$base_parameters);
+if($base_parameters == "") {
+    $sort = '?sort_by=';
+}
+else {
+    $sort = '&sort_by=';
+}
+?>
 
-        <span class="sort">
-            <strong>Sort by</strong>
-            <?php foreach($sort_options as $label => $field) {
-                if($label == 'Relevancy')
-                {
-                    ?>
-                    <em><a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc'?>"><?php echo $label ?></a></em>
+<div class="col-md-9 col-sm-9 col-xs-12">
+    <div class="row">
+        <div class="centered text-center">
+            <nav>
+                <ul class="pagination pagination-sm pagination-xs">
                     <?php
-                }
-                else {
-            ?>
+                    foreach ($paginationlinks as $pagelink)
+                    {
+                        echo $pagelink;
+                    }
+                    ?>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="row search-row">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 results-num">
+            <h5 class="text-muted">Showing <?php echo $rows ?> results </h5>
+        </div>
 
-                <em><?php echo $label ?></em>
-                <?php if($label != "Date") { ?>
-                <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+asc' ?>">A-Z</a> |
-                <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc' ?>">Z-A</a>
-            <?php } else { ?>
-                <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc' ?>">newest</a> |
-                <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+asc' ?>">oldest</a>
-          <?php } } } ?>
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 results-num sort">
+            <h5 class="text-muted">Sort By:
+                <?php foreach($sort_options as $label => $field) {
+                    if($label == 'Relevancy')
+                    {
+                        ?>
+                        <em><a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc'?>"><?php echo $label ?></a></em>
+                        <?php
+                    }
+                    else {
+                        ?>
 
-        </span>
+                        <em><?php echo $label ?></em>
+                        <?php if($label != "Date") { ?>
+                            <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+asc' ?>">A-Z</a> |
+                            <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc' ?>">Z-A</a>
+                        <?php } else { ?>
+                            <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+desc' ?>">newest</a> |
+                            <a href="<?php echo $base_search.$base_parameters.$sort.$field.'+asc' ?>">oldest</a>
+                        <?php } } } ?>
+            </h5>
+        </div>
 
     </div>
-
-
-    <ul class="listing">
-
-
-    <?php foreach ($docs as $index => $doc) {
+    <?php
+    foreach ($docs as $index => $doc) {
         ?>
-        <?php
-        $type = 'Unknown';
-
-        if(isset($doc[$type_field])) {
-                    $type = "media-" . strtolower(str_replace(' ','-',$doc[$type_field][0]));
-                }
-        ?>
-
-    <li<?php if($index == 0) { echo ' class="first"'; } elseif($index == sizeof($docs) - 1) { echo ' class="last"'; } ?>>
-
-
-        <div class = "iteminfo">
-            <h3><a href="./record/<?php echo $doc['id']?>?highlight=<?php echo $query ?>"><?php echo $doc[$title_field][0]; ?></a></h3>
-            <div class="tags">
-
-
-        <?php if(array_key_exists($author_field,$doc)) { ?>
+        <div class="row search-row">
+            <h3><a href="./record/<?php echo $doc['id']?>/<?php echo $doc['types'][0]?>"><?php echo $doc[$title_field]; ?></a></h3>
 
             <?php
-            $num_authors = 0;
-            foreach ($doc[$author_field] as $author) {
+            if (isset($doc["component_id"])) {
+                $component_id = $doc["component_id"];
+                echo'<div class="component_id">' . $component_id . '</div>';
+            } ?>
 
-               // test author linking
-               // quick hack that only works if the filter key
-               // and recorddisplay key match and the delimiter is :
-                $orig_filter = ucwords(urlencode($author));
-
-                $lower_orig_filter = strtolower($author);
-                $lower_orig_filter = urlencode($lower_orig_filter);
-                echo '<a href="./search/*:*/Author:%22'.$lower_orig_filter.'%7C%7C%7C'.$orig_filter.'%22">'.$author.'</a>';
-                $num_authors++;
-                if($num_authors < sizeof($doc[$author_field])) {
-                    echo ' ';
-                }
-            }
-         } ?>
-
-
-       <?php if(array_key_exists($date_field, $doc)) { ?>
-            <span>
+            <?php if(array_key_exists($author_field,$doc)) { ?>
                 <?php
-                echo '(' . $doc[$date_field][0] . ')';
-          } ?>
-                </span>
 
+                $num_authors = 0;
+                foreach ($doc[$author_field] as $author) {
+                    $orig_filter = urlencode($author);
 
-        </div> <!-- close tags div -->
-            <?php if(isset($doc[$bitstream_field])) //&& $link_bitstream)
-            {
+                    echo '<a class="agent" href="./search/*:*/Agent:%22'.$orig_filter.'%22">'.$author.'</a>';
+                    $num_authors++;
+                    if($num_authors < sizeof($doc[$author_field])) {
+                        echo ' ';
+                    }
+                }
                 ?>
-                <div class="record-bitstreams">
+            <?php } ?>
 
+            <?php if(array_key_exists($subject_field,$doc)) { ?>
+                <div class="tags">
                     <?php
-                    $pdfcount =  0;
-                    foreach($doc[$bitstream_field] as $bitstream)
-                    {
-                        $bitstreamLink = $this->skylight_utilities->getBitstreamURI($bitstream);
-                        if (strpos($bitstreamLink, ".pdf") > 0)
-                        {
-                            $pdfcount = $pdfcount + 1;
+
+                    $num_subject = 0;
+                    foreach ($doc[$subject_field] as $subject) {
+
+                        $orig_filter = urlencode($subject);
+                        echo '<a class="subject" href="./search/*:*/Subject:%22'.$orig_filter.'%22">'.$subject.'</a>';
+                        $num_subject++;
+                        if($num_subject < sizeof($doc[$subject_field])) {
+                            echo ' ';
                         }
                     }
 
-                    if ($pdfcount > 1)
-                    {
-                        echo 'Multiple PDFs. Open the record to view them all.';
-                    }
-                    else if ($pdfcount == 1)
-                    {
-                        foreach($doc[$bitstream_field] as $bitstream)
-                        {
-                            if (strpos($bitstreamLink, ".pdf") > 0) {
-                                $bitstreamLink = $this->skylight_utilities->getBitstreamURI($bitstream);
-                                echo '<a href="' . $bitstreamLink . '" target= "_blank" class="downloadButton">Download  PDF</a>';
-                            }
-                        }
-                    }
-                    else
-                    {
-                        echo '<div class="record-bitstreams"><a href="./unavailable" title="Click here to find out why this book may be unavailable">Book unavailable</a></div>';
-                    }
                     ?>
                 </div>
-
-            <?php
-            }
-            else { ?>
-                <div class="record-bitstreams"><a href="./unavailable" title="Click here to find out why this paper may be unavailable">Paper unavailable</a></div>
             <?php } ?>
-            <p><?php
-                echo 'Shelfmark: '.$doc[$shelfmark_field][0]; ?></p>
+        </div> <!-- close row-->
+        <?php
+
+    } // end for each search result
+
+    ?>
+    <div class="row">
+        <div class="centered text-center">
+            <nav>
+                <ul class="pagination pagination-sm pagination-xs">
+                    <?php
+                    foreach ($paginationlinks as $pagelink)
+                    {
+                        echo $pagelink;
+                    }
+                    ?>
+                </ul>
+            </nav>
         </div>
-    </li>
-        <?php }?>
-    </ul>
-    <div class="pagination">
-       <?php echo $pagelinks ?>
     </div>
+</div> <!-- close col 9 -->
