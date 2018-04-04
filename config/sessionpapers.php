dbc66d9598587b8424d053f9cdf0ccac2981d13a
Mike Bennett <mike.bennett@ed.ac.uk>
2018-04-04 11:56:04 +0100

    First pass config file

--- config/sessionpapers.php
+++ config/sessionpapers.php
@@ -1,63 +1,62 @@
 <?php
 
-$config['skylight_appname'] = 'Default';
+$config['skylight_appname'] = 'sessionpapers';
 
-$config['skylight_theme'] = 'default';
+$config['skylight_theme'] = 'sessionpapers';
 
-$config['skylight_fullname'] = 'Research Space';
+$config['skylight_fullname'] = 'Scottish Court of Session Papers';
 
 $config['skylight_adminemail'] = 'lddt@mlist.is.ed.ac.uk';
 
-$config['skylight_oaipmhcollection'] = 'hdl_123456789_1';
+$config['skylight_oaipmhcollection'] = ''; # TODO: Obtain / set this
 
+// Uncomment this if you are using a url of the form http://.../art/...
+$config['skylight_url_prefix'] = 'sessionpapers';
 
 // Container ID and the field used in solr index to store this ID. Used for restricting search/browse scope.
-$config['skylight_container_id'] = '1';
+$config['skylight_container_id'] = ''; # TODO: Obtain / set this
 $config['skylight_container_field'] = 'location.comm';
 
 $config['skylight_sitemap_type'] = 'internal';
 
+
 $config['skylight_fields'] = array('Title' => 'dc.title.en',
-    'Author' => 'dc.contributor.author',
-    'Subject' => 'dc.subject.en',
-    'Type' => 'dc.type.en',
-    'Abstract' => 'dc.description.abstract',
-    'Date' => 'dc.date.issued',
-    'Accession Date' => 'dc.date.accessioned_dt',
-    'Handle' => 'dc.identifier.uri'
+    'Reference' => 'dc.identifier.other',
+    'Link' => 'dc.identifier.uri',
+    'Date' => 'dc.coverage.temporal',
+    'Bitstream'=> 'dc.format.original.en',
+    'Thumbnail'=> 'dc.format.thumbnail.en',
+    'Description'=>'dc.description.en',
+    'Format' => 'dc.format.en',
+    'Shelf Mark' => 'dc.identifier.other'
 );
 
+
 $config['skylight_date_filters'] = array('Date' => 'dateIssued.year_sort');
-$config['skylight_filters'] = array('Author' => 'author_filter', 'Subject' => 'subject_filter', 'Type' => 'type_filter');
+$config['skylight_filters'] = array('Subject' => 'subject_filter');
 $config['skylight_filter_delimiter'] = ':';
 
 $config['skylight_meta_fields'] = array('Title' => 'dc.title',
-    'Author' => 'dc.creator',
-    'Abstract' => 'dc.description.abstract',
     'Subject' => 'dc.subject',
     'Date' => 'dc.date.issued',
-    'Type' => 'dc.type');
+);
 
-$config['skylight_recorddisplay'] = array('Title','Author','Subject','Type','Abstract');
+$config['skylight_recorddisplay'] = array('Title','Subject');
 
-$config['skylight_searchresult_display'] = array('Title','Author','Subject','Type','Abstract');
+$config['skylight_searchresult_display'] = array('Title','Subject');
 
 $config['skylight_search_fields'] = array('Keywords' => 'text',
-    'Subject' => 'dc.subject',
-    'Type' => 'dc.type',
-    'Author' => 'dc.creator'
+    'Subject' => 'dc.subject'
 );
 
 $config['skylight_sort_fields'] = array('Title' => 'dc.title',
-    'Date' => 'dc.date.issued_dt',
-    'Author' => 'dc.creator'
+    'Date' => 'dc.date.issued_dt'
 );
 
 $config['skylight_feed_fields'] = array('Title' => 'Title',
     'Author' => 'Author',
-    'Subject' => 'Subject',
-    'Description' => 'Abstract',
-    'Date' => 'Date');
+    'Subject' => 'Subject'
+);
 
 $config['skylight_results_per_page'] = 10;
 $config['skylight_share_buttons'] = false;
